a0322d32fd7cb6f4aaf9d9eb65acc22769525446
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-17 20:13:26 +0530

    feat: add func to display func

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Review;
 use Illuminate\Http\Request;
 
 class ReviewController extends Controller
 {
-    //
+    public function display($id)
+    {
+        $review = Review::where("product_id", $id)->get();
+        return response()->json(["review" => $review], 200);
+    }
+
+    
 }
