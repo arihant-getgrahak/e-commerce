edd3175e39f8102bb2b2a52bc2dfc94d71b9891e
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-17 19:51:21 +0530

    feat: add migration for product mets

--- database/migrations/2024_10_17_141708_create_product_metas_table.php
+++ database/migrations/2024_10_17_141708_create_product_metas_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('product_metas', function (Blueprint $table) {
             $table->id();
+            $table->string('color');
+            $table->string('size');
+            $table->string('weight');
             $table->timestamps();
         });
     }
