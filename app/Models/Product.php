34dcb306f51f7961eb3cac6232e7abcce559b31c
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-17 17:07:06 +0530

    add fillable for products

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+        "name",
+        "description",
+        "price",
+        "stock",
+        "parent_category_id",
+        "child_category_id",
+        "added_by",
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            "created_at" => "datetime:Y-m-d",
+            "updated_at" => "datetime:Y-m-d",
+        ];
+    }
 }
