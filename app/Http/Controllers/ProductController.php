7b4c7b5190c6c6c855c8e11fa106348b26cfb576
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-17 17:33:51 +0530

    add controller to delete product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -46,5 +46,17 @@ class ProductController extends Controller
         ], 200);
     }
 
-    
+    public function delete($id)
+    {
+        $product = Product::find($id);
+        if (!$product) {
+            return response()->json([
+                "message" => "Product not found"
+            ], 404);
+        }
+        $product->delete();
+        return response()->json([
+            "message" => "Product deleted successfully"
+        ], 200);
+    }
 }
