6ee833cde3215667e2ccf69202b6ed7f79d3eafa
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-17 17:34:42 +0530

    add route to update and delete product

--- routes/api.php
+++ routes/api.php
@@ -7,4 +7,6 @@ use App\Http\Controllers\ProductController;
 Route::group(["prefix" => "product"], function () {
     Route::get("/", [ProductController::class, "display"]);
     Route::post("/store", [ProductController::class, "store"]);
+    Route::post("/update/{id}", [ProductController::class, "update"]);
+    Route::delete("/delete/{id}", [ProductController::class, "delete"]);
 });
