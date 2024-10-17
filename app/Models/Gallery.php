de3f1c8e117d8d7e08da763d0c0ba0514f14e0a5
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-17 17:14:19 +0530

    add gallery model

---
+++ app/Models/Gallery.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Gallery extends Model
+{
+    protected $fillable = [
+        'product_id',
+        'image',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            "created_at" => "datetime:Y-m-d",
+            "updated_at" => "datetime:Y-m-d",
+        ];
+    }
+}
