c2b7757848de0ebaf6305eb9114bb600e17c60df
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-17 17:09:36 +0530

    add controller to display all product

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    // public function index(){
+    //     return view("product");
+    // }
+
+    public function display(){
+        $product = Product::all();
+        return view("product", compact("product"));
+    }
+}
