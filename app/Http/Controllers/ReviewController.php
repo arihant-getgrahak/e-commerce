84dc18d1cc857fcefbd3404b6228a72cb579b51a
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-17 20:18:32 +0530

    feat: add func to save data to db

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Review;
 use Illuminate\Http\Request;
+use Validator;
 
 class ReviewController extends Controller
 {
@@ -13,5 +14,30 @@ class ReviewController extends Controller
         return response()->json(["review" => $review], 200);
     }
 
-    
+    public function store(Request $request)
+    {
+        $validate = Validator::make($request->all(), [
+            "product_id" => "required|exists:products,id",
+            "comment" => "required",
+            "rating" => "required",
+            "name" => "required",
+            "email" => "required|email",
+        ]);
+
+        if ($validate->fails()) {
+            return response()->json(["error" => $validate->errors()], 400);
+        }
+
+        $data = [
+            "product_id" => $request->product_id,
+            "comment" => $request->comment,
+            "name" => $request->name,
+            "email" => $request->email,
+            "rating" => $request->rating,
+        ];
+
+        $review = Review::create($data);
+
+        return response()->json(["review" => $review], 200);
+    }
 }
