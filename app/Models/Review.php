9a45372a002cfbad12d75218844244e69732e29a
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-17 20:10:47 +0530

    feat: add fillable in Review model

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    protected $fillable = [
+        "name",
+        "rating",
+        "email",
+        "comment",
+        "product_id"
+    ];
 }
