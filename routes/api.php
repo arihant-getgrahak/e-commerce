719135dba6a314aa7fb3d6048ce045bb8bfc9da8
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-17 17:18:34 +0530

    make api route

---
+++ routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+
+Route::group(["prefix" => "product"], function () {
+    Route::get("/", [ProductController::class, "display"]);
+});
