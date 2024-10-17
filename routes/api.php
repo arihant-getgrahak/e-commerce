e41b227985ea480c7d62575e30477d7d663ad376
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-17 17:36:34 +0530

    add route to show specific product

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\ProductController;
 
 Route::group(["prefix" => "product"], function () {
     Route::get("/", [ProductController::class, "display"]);
+    Route::get("/{id}", [ProductController::class,"specific"]);
     Route::post("/store", [ProductController::class, "store"]);
     Route::post("/update/{id}", [ProductController::class, "update"]);
     Route::delete("/delete/{id}", [ProductController::class, "delete"]);
