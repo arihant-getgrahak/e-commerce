8809923db20eef7953b8863ae912e24c5d22b2f6
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-17 18:58:26 +0530

    feat: add controller to delete category

--- app/Http/Controllers/ChildCategoryController.php
+++ app/Http/Controllers/ChildCategoryController.php
@@ -63,4 +63,21 @@ class ChildCategoryController extends Controller
             "category" => $category
         ], 200);
     }
+
+    public function delete($id)
+    {
+        $category = ChildCategory::find($id);
+        if (!$category) {
+            return response()->json([
+                "status" => false,
+                "message" => "Category not found"
+            ], 404);
+        }
+
+        $category->delete();
+        return response()->json([
+            "status" => true,
+            "message" => "Category deleted successfully"
+        ], status: 200);
+    }
 }
