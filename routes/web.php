35fce49a3845c281f1c8b20d3d4a9428b16fbc3a
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-18 16:45:26 +0530

    feat: add routes for view

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('admin', function () {
+    return view('admin');
+});
+
+Route::get('category', function () {
+    return view('addcategroy');
+});
+
+Route::get('product/add', function () {
+    return view('addproduct');
+});
+
+Route::get('product/view', function () {
+    return view('productview');
+});
+
+
+
+
