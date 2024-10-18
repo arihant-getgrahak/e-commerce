6074cdca37c4cd0855c03c7a4a5145c2597f1e65
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-18 21:47:49 +0530

    feat: add func to user register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Auth;
 use Illuminate\Http\Request;
+use Validator;
 
 class AuthController extends Controller
 {
@@ -19,5 +21,34 @@ class AuthController extends Controller
         }
 
         return redirect()->route("admin");
+    }
+
+    public function register(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            "name" => "required",
+            "email" => "required|email|unique:users,email",
+            "password" => "required|min:6",
+        ]);
+
+        if ($validator->fails()) {
+            return back()->withErrors($validator)->withInput();
+        }
+
+        $user = User::create([
+            "name" => $request->name,
+            "email" => $request->email,
+            "password" => bcrypt($request->password),
+        ]);
+
+        if (!$user) {
+            return back()->with("error", "Something went wrong");
+        }
+
+        return response()->json([
+            "message" => "User created successfully",
+            "user" => $user
+        ]);
     }
 }
