e8cad4c12aa06ea7cd3e5caba1f5d8c97635d816
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-19 13:16:51 +0530

    fix: make product list dynamic

--- resources/views/productlist.blade.php
+++ resources/views/productlist.blade.php
@@ -8,325 +8,63 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body>
-<div class="w-[1190px] mx-auto my-auto">
-  <div class="flex flex-col w-full h-auto mt-[3%]">
-    <div class="flex flex-wrap">
-      
-      <!-- product #1 -->
-      <div class="grow max-w-[20%] inline-flex px-2 pb-4 min-w-[20%]">
-        <a href="#" class="relative bg-white inline-flex items-stretch w-full h-full p-0 box-border">
-          <!-- heart -->
-          <div class="absolute top-[4px] right-[4px] w-[28px] h-[28px] text-center z-10 rounded-full">
-            <div>
-              <span class="absolute top-[4px] right-[4px] w-[24px] h-[24px] text-center z-10 rounded-full bg-gray-200">
-                <div class="absolute top-0 left-0 right-0 bottom-0 m-auto">
-                  <i class="fa-solid fa-heart" style="color: #cc3535;"></i>
+<body class="bg-gray-300">
+    <header class="bg-white">
+        <div class="container mx-auto px-6 py-3">
+            <div class="flex items-center justify-between">
+                <div class="w-full text-gray-700 md:text-center text-2xl font-semibold">
+                    Arihant Jain
                 </div>
-              </span>
             </div>
-          </div>
-          <!-- end heart -->
-          <div class="relative shadow-md rounded-lg min-w-full flex flex-col">
-            <!-- img product -->
-            <div class="relative">
-              <div class="relative h-0 rounded-t-lg pb-[100%] w-full bg-white text-ellipsis	overflow-hidden"> 
-                <div class="bg-no-repeat bg-cover inline-block my-0 mx-auto text-center w-full h-full absolute">
-                  <img src="https://fakeimg.pl/400x400/a5edc0/616161?text=product" alt="">
+        </div>
+    </header>
+    <div class="w-[1190px] mx-auto my-auto">
+        <div class="flex flex-col w-full h-auto mt-[3%]">
+            @if (!$product->isEmpty())
+                <div class="flex flex-wrap">
+                    @foreach ($product as $p)
+                        <div class="grow max-w-[20%] inline-flex px-2 pb-4 min-w-[20%]">
+                            <a href="#" class="relative bg-white inline-flex items-stretch w-full h-full p-0 box-border">
+                                <div class="relative shadow-md rounded-lg min-w-full flex flex-col">
+                                    <div class="relative">
+                                        <div class="relative h-0 pb-[100%] w-full bg-white text-ellipsis	overflow-hidden">
+                                            <div
+                                                class="bg-no-repeat bg-cover inline-block my-0 mx-auto text-center w-full h-full absolute">
+                                                @if($p->gallery && $p->gallery->isNotEmpty())
+                                                    <img src="{{ $p->gallery[0]->image }}" alt="Product Image"
+                                                        class="w-full h-48 object-cover transition-transform duration-500 transform group-hover:scale-105">
+                                                @else
+                                                    <img src="https://via.placeholder.com/150" alt="Default Image"
+                                                        class="w-full h-48 object-cover">
+                                                @endif
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="relative p-2 box-border overflow-hidden grow">
+                                        <span
+                                            class="text-gray-800 max-h-[38px] text-sm font-semibold leading-5 overflow-hidden whitespace-normal break-words		">
+                                            {{ $p->name }}
+                                        </span>
+                                        <div>
+                                            <div class="block w-full text-sm font-bold text-amber-700">
+                                                {{ $p->price }}
+                                            </div>
+                                        </div>
+                                        <div class="flex align-items-center text-xs leading-6 mx-0 my-1 text-gray-600">
+                                            {{ $p->brand->name }}
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    @endforeach
                 </div>
-              </div>
-            </div>
-            <!-- end img product -->
-            <div class="relative p-2 box-border overflow-hidden grow">
-              <!-- product name -->
-              <span class="text-gray-800 max-h-[38px] text-sm font-semibold leading-5 overflow-hidden whitespace-normal break-words		">
-                iPhone 14 Pro Max
-              </span>
-              <!-- end product name -->
-              <!-- price -->
-              <div>
-                <div class="block w-full text-sm font-bold text-amber-700">
-                  $ 1900
-                </div>
-              </div>
-              <!-- end price -->
-              <!-- store name -->
-              <div class="flex align-items-center text-xs leading-6 mx-0 my-1 text-gray-600">
-                iBox
-              </div>
-              <!-- end store name -->
-              <!-- rating -->
-              <div class="min-h-[14px] text-xs">
-                <div class="align-items-center my-1 mx-0">
-                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
-                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
-                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
-                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
-                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
-                  <span class="align-midle ml-1 text-gray-600">
-                    (1400)
-                  </span>
-                </div>
-              </div>
-              <!-- end rating -->
-              
-            </div>
-          </div>
-        </a>
-      </div>
-      
-      <!-- product #2 -->
-      <div class="grow max-w-[20%] inline-flex px-2 pb-4 min-w-[20%]">
-        <a href="#" class="relative bg-white inline-flex items-stretch w-full h-full p-0 box-border">
-          <!-- heart -->
-          <div class="absolute top-[4px] right-[4px] w-[28px] h-[28px] text-center z-10 rounded-full">
-            <div>
-              <span class="absolute top-[4px] right-[4px] w-[24px] h-[24px] text-center z-10 rounded-full bg-gray-200">
-                <div class="absolute top-0 left-0 right-0 bottom-0 m-auto">
-                  <i class="fa-solid fa-heart" style="color: #949494;"></i>
-                </div>
-              </span>
-            </div>
-          </div>
-          <!-- end heart -->
-          <div class="relative shadow-md rounded-lg min-w-full flex flex-col">
-            <!-- img product -->
-            <div class="relative">
-              <div class="relative h-0 rounded-t-lg pb-[100%] w-full bg-white text-ellipsis	overflow-hidden"> 
-                <div class="bg-no-repeat bg-cover inline-block my-0 mx-auto text-center w-full h-full absolute">
-                  <img src="https://fakeimg.pl/400x400/a5edc0/616161?text=product" alt="">
-                </div>
-              </div>
-            </div>
-            <!-- end img product -->
-            <div class="relative p-2 box-border overflow-hidden grow">
-              <!-- product name -->
-              <span class="text-gray-800 max-h-[38px] text-sm font-semibold leading-5 overflow-hidden whitespace-normal break-words		">
-                iPhone 14 Pro Max
-              </span>
-              <!-- end product name -->
-              <!-- price -->
-              <div>
-                <div class="block w-full text-sm font-bold text-amber-700">
-                  $ 1900
-                </div>
-              </div>
-              <!-- end price -->
-              <!-- store name -->
-              <div class="flex align-items-center text-xs leading-6 mx-0 my-1 text-gray-600">
-                iBox
-              </div>
-              <!-- end store name -->
-              <!-- rating -->
-              <div class="min-h-[14px] text-xs">
-                <div class="align-items-center my-1 mx-0">
-                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
-                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
-                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
-                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
-                  <i class="fa-solid fa-star-half" style="color: #7a7600;"></i>
-                  <span class="align-midle ml-1 text-gray-600">
-                    (1400)
-                  </span>
-                </div>
-              </div>
-              <!-- end rating -->
-              
-            </div>
-          </div>
-        </a>
-      </div>
-      
-      <!-- product #3 -->
-      <div class="grow max-w-[20%] inline-flex px-2 pb-4 min-w-[20%]">
-        <a href="#" class="relative bg-white inline-flex items-stretch w-full h-full p-0 box-border">
-          <!-- heart -->
-          <div class="absolute top-[4px] right-[4px] w-[28px] h-[28px] text-center z-10 rounded-full">
-            <div>
-              <span class="absolute top-[4px] right-[4px] w-[24px] h-[24px] text-center z-10 rounded-full bg-gray-200">
-                <div class="absolute top-0 left-0 right-0 bottom-0 m-auto">
-                  <i class="fa-solid fa-heart" style="color: #949494;"></i>
-                </div>
-              </span>
-            </div>
-          </div>
-          <!-- end heart -->
-          <div class="relative shadow-md rounded-lg min-w-full flex flex-col">
-            <!-- img product -->
-            <div class="relative">
-              <div class="relative h-0 rounded-t-lg pb-[100%] w-full bg-white text-ellipsis	overflow-hidden"> 
-                <div class="bg-no-repeat bg-cover inline-block my-0 mx-auto text-center w-full h-full absolute">
-                  <img src="https://fakeimg.pl/400x400/a5edc0/616161?text=product" alt="">
-                </div>
-              </div>
-            </div>
-            <!-- end img product -->
-            <div class="relative p-2 box-border overflow-hidden grow">
-              <!-- product name -->
-              <span class="text-gray-800 max-h-[38px] text-sm font-semibold leading-5 overflow-hidden whitespace-normal break-words		">
-                iPhone 14 Pro Max
-              </span>
-              <!-- end product name -->
-              <!-- price -->
-              <div>
-                <div class="block w-full text-sm font-bold text-amber-700">
-                  $ 1900
-                </div>
-              </div>
-              <!-- end price -->
-              <!-- store name -->
-              <div class="flex align-items-center text-xs leading-6 mx-0 my-1 text-gray-600">
-                iBox
-              </div>
-              <!-- end store name -->
-              <!-- rating -->
-              <div class="min-h-[14px] text-xs">
-                <div class="align-items-center my-1 mx-0">
-                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
-                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
-                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
-                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
-                  <i class="fa-regular fa-star" style="color: #7a7600;"></i>
-                  <span class="align-midle ml-1 text-gray-600">
-                    (1400)
-                  </span>
-                </div>
-              </div>
-              <!-- end rating -->
-              
-            </div>
-          </div>
-        </a>
-      </div>
-      
-      <!-- product #4 -->
-      <div class="grow max-w-[20%] inline-flex px-2 pb-4 min-w-[20%]">
-        <a href="#" class="relative bg-white inline-flex items-stretch w-full h-full p-0 box-border">
-          <!-- heart -->
-          <div class="absolute top-[4px] right-[4px] w-[28px] h-[28px] text-center z-10 rounded-full">
-            <div>
-              <span class="absolute top-[4px] right-[4px] w-[24px] h-[24px] text-center z-10 rounded-full bg-gray-200">
-                <div class="absolute top-0 left-0 right-0 bottom-0 m-auto">
-                  <i class="fa-solid fa-heart" style="color: #949494;"></i>
-                </div>
-              </span>
-            </div>
-          </div>
-          <!-- end heart -->
-          <div class="relative shadow-md rounded-lg min-w-full flex flex-col">
-            <!-- img product -->
-            <div class="relative">
-              <div class="relative h-0 rounded-t-lg pb-[100%] w-full bg-white text-ellipsis	overflow-hidden"> 
-                <div class="bg-no-repeat bg-cover inline-block my-0 mx-auto text-center w-full h-full absolute">
-                  <img src="https://fakeimg.pl/400x400/a5edc0/616161?text=product" alt="">
-                </div>
-              </div>
-            </div>
-            <!-- end img product -->
-            <div class="relative p-2 box-border overflow-hidden grow">
-              <!-- product name -->
-              <span class="text-gray-800 max-h-[38px] text-sm font-semibold leading-5 overflow-hidden whitespace-normal break-words		">
-                iPhone 14 Pro Max
-              </span>
-              <!-- end product name -->
-              <!-- price -->
-              <div>
-                <div class="block w-full text-sm font-bold text-amber-700">
-                  $ 1900
-                </div>
-              </div>
-              <!-- end price -->
-              <!-- store name -->
-              <div class="flex align-items-center text-xs leading-6 mx-0 my-1 text-gray-600">
-                iBox
-              </div>
-              <!-- end store name -->
-              <!-- rating -->
-              <div class="min-h-[14px] text-xs">
-                <div class="align-items-center my-1 mx-0">
-                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
-                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
-                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
-                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
-                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
-                  <span class="align-midle ml-1 text-gray-600">
-                    (1400)
-                  </span>
-                </div>
-              </div>
-              <!-- end rating -->
-              
-            </div>
-          </div>
-        </a>
-      </div>
-      
-      <!-- product #5 -->
-      <div class="grow max-w-[20%] inline-flex px-2 pb-4 min-w-[20%]">
-        <a href="#" class="relative bg-white inline-flex items-stretch w-full h-full p-0 box-border">
-          <!-- heart -->
-          <div class="absolute top-[4px] right-[4px] w-[28px] h-[28px] text-center z-10 rounded-full">
-            <div>
-              <span class="absolute top-[4px] right-[4px] w-[24px] h-[24px] text-center z-10 rounded-full bg-gray-200">
-                <div class="absolute top-0 left-0 right-0 bottom-0 m-auto">
-                  <i class="fa-solid fa-heart" style="color: #949494;"></i>
-                </div>
-              </span>
-            </div>
-          </div>
-          <!-- end heart -->
-          <div class="relative shadow-md rounded-lg min-w-full flex flex-col">
-            <!-- img product -->
-            <div class="relative">
-              <div class="relative h-0 rounded-t-lg pb-[100%] w-full bg-white text-ellipsis	overflow-hidden"> 
-                <div class="bg-no-repeat bg-cover inline-block my-0 mx-auto text-center w-full h-full absolute">
-                  <img src="https://fakeimg.pl/400x400/a5edc0/616161?text=product" alt="">
-                </div>
-              </div>
-            </div>
-            <!-- end img product -->
-            <div class="relative p-2 box-border overflow-hidden grow">
-              <!-- product name -->
-              <span class="text-gray-800 max-h-[38px] text-sm font-semibold leading-5 overflow-hidden whitespace-normal break-words		">
-                iPhone 14 Pro Max
-              </span>
-              <!-- end product name -->
-              <!-- price -->
-              <div>
-                <div class="block w-full text-sm font-bold text-amber-700">
-                  $ 1900
-                </div>
-              </div>
-              <!-- end price -->
-              <!-- store name -->
-              <div class="flex align-items-center text-xs leading-6 mx-0 my-1 text-gray-600">
-                iBox
-              </div>
-              <!-- end store name -->
-              <!-- rating -->
-              <div class="min-h-[14px] text-xs">
-                <div class="align-items-center my-1 mx-0">
-                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
-                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
-                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
-                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
-                  <i class="fa-solid fa-star" style="color: #7a7600;"></i>
-                  <span class="align-midle ml-1 text-gray-600">
-                    (1400)
-                  </span>
-                </div>
-              </div>
-              <!-- end rating -->
-              
-            </div>
-          </div>
-        </a>
-      </div>
+            @else
+                <h1>No Products</h1>
+            @endif
+        </div>
     </div>
 
-  </div>
-</div>
-    
 </body>
 
 </html>
