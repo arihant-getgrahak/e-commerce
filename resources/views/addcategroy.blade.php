2b21df5ff6494f1a7fdfa0c505fb4ff4e27d5fcd
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-21 13:59:38 +0530

    fix: update add category ui

--- resources/views/addcategroy.blade.php
+++ resources/views/addcategroy.blade.php
@@ -1,15 +1,19 @@
 @extends('layout.index')
+
+
 @section("category")
 <main class="w-3/4 p-8">
+    
     <h2 class="text-3xl font-bold mb-4">Add Category</h2>
     <form class="bg-white p-6 rounded-lg shadow-lg" action="{{ route('category.add') }}" method="POST">
         @csrf
-        <div class="mb-4">
-            <label for="name" class="block text-sm font-medium text-gray-700">Category Name</label>
-            <input type="text" id="name" name="name" class="mt-1 block w-full p-2 border border-gray-300 rounded-md"
-                placeholder="Enter category name">
+        <div class="mb-3">
+            <label class="form-label required">Category Name</label>
+            <input type="text" class="form-control" id="name" name="name" placeholder="Enter category name">
         </div>
-        <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded-md">Add Category</button>
+        <button type="submit" class="btn btn-primary ms-auto">
+            Add Category
+        </button>
     </form>
 </main>
 <script>
