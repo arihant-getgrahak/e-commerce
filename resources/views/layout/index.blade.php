127d21d164c532d021bb0134f59cb300041da3a3
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-21 13:47:22 +0530

    fix: update layout/index file

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -5,18 +5,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Panel</title>
-    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+    <!-- @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     @else
         <script src="https://cdn.tailwindcss.com"></script>
-    @endif
+    @endif -->
+
+    <!-- @yield("header") -->
+    <link rel="stylesheet" href="{{asset('dist/css/tabler.min.css')}}">
+    <link rel="stylesheet" href="{{asset('dist/css/tabler-flags.min.css')}}">
+    <link rel="stylesheet" href="{{asset('dist/css/tabler-payments.min.css')}}">
+    <link rel="stylesheet" href="{{asset('dist/css/tabler-vendors.min.css')}}">
+    <link rel="stylesheet" href="{{asset('dist/css/demo.min.css')}}">
+
+    <style>
+        @import url('https://rsms.me/inter/inter.css');
+
+        :root {
+            --tblr-font-sans-serif: 'Inter Var', -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue, sans-serif;
+        }
+
+        body {
+            font-feature-settings: "cv03", "cv04", "cv11";
+        }
+    </style>
 
 </head>
 
 <body class="bg-gray-100">
-    <header>This is header</header>
-    <div class="flex">
-        <aside class="w-1/4 bg-gray-800 text-white h-screen p-5">
+    <!-- <header>This is header</header> -->
+    <div>
+        <!-- <aside class="w-1/4 bg-gray-800 text-white h-screen p-5">
             <h1 class="text-2xl font-bold mb-6">Admin Panel</h1>
             <nav>
                 <ul>
@@ -37,7 +56,118 @@
                     </li>
                 </ul>
             </nav>
-        </aside>
+        </aside> -->
+
+        <header class="navbar-expand-md">
+            <div class="collapse navbar-collapse" id="navbar-menu">
+                <div class="navbar">
+                    <div class="container-xl">
+                        <ul class="navbar-nav">
+                            <!-- home -->
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('admin') }}">
+                                    <span
+                                        class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/home -->
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
+                                            viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                                            stroke-linecap="round" stroke-linejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                            <path d="M5 12l-2 0l9 -9l9 9l-2 0" />
+                                            <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" />
+                                            <path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" />
+                                        </svg>
+                                    </span>
+                                    <span class="nav-link-title"> Home </span>
+                                </a>
+                            </li>
+
+                            <!-- add product -->
+                            <li class="nav-item active">
+                                <a class="nav-link" href="{{ route('product.add') }}">
+                                    <span
+                                        class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/checkbox -->
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
+                                            viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                                            stroke-linecap="round" stroke-linejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                            <path d="M9 11l3 3l8 -8" />
+                                            <path
+                                                d="M20 12v6a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h9" />
+                                        </svg>
+                                    </span>
+                                    <span class="nav-link-title"> Add Product </span>
+                                </a>
+                            </li>
+
+                            <!-- view product -->
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ url('product/view') }}">
+                                    <span class="nav-link-icon d-md-none d-lg-inline-block">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                            stroke-linecap="round" stroke-linejoin="round"
+                                            class="icon icon-tabler icons-tabler-outline icon-tabler-binoculars">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                            <path d="M7 16m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+                                            <path d="M17 16m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+                                            <path
+                                                d="M16.346 9.17l-.729 -1.261c-.16 -.248 -1.056 -.203 -1.117 .091l-.177 1.38" />
+                                            <path
+                                                d="M19.761 14.813l-2.84 -5.133c-.189 -.31 -.592 -.68 -1.421 -.68c-.828 0 -1.5 .448 -1.5 1v6" />
+                                            <path
+                                                d="M7.654 9.17l.729 -1.261c.16 -.249 1.056 -.203 1.117 .091l.177 1.38" />
+                                            <path
+                                                d="M4.239 14.813l2.84 -5.133c.189 -.31 .592 -.68 1.421 -.68c.828 0 1.5 .448 1.5 1v6" />
+                                            <rect width="4" height="2" x="10" y="12" />
+                                        </svg>
+                                    </span>
+
+                                    <span class="nav-link-title"> View Product </span>
+                                </a>
+                            </li>
+
+                            <!-- add category -->
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ url('product/category') }}">
+                                    <span
+                                        class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/home -->
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                            stroke-linecap="round" stroke-linejoin="round"
+                                            class="icon icon-tabler icons-tabler-outline icon-tabler-category">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                            <path d="M4 4h6v6h-6z" />
+                                            <path d="M14 4h6v6h-6z" />
+                                            <path d="M4 14h6v6h-6z" />
+                                            <path d="M17 17m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+                                        </svg>
+                                    </span>
+                                    <span class="nav-link-title"> Add Category </span>
+                                </a>
+                            </li>
+                        </ul>
+                        <div class="my-2 my-md-0 flex-grow-1 flex-md-grow-0 order-first order-md-last">
+                            <a class="nav-link" href='/arihant/e-commerce/public/logout'>
+                                <span
+                                    class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/mail-opened -->
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        class="icon icon-tabler icons-tabler-outline icon-tabler-logout">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                        <path
+                                            d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />
+                                        <path d="M9 12h12l-3 -3" />
+                                        <path d="M18 15l3 -3" />
+                                    </svg>
+                                </span>
+                                <span class="nav-link-title"> Logout </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
 
         <main class="w-3/4 p-8">
             <main>
@@ -55,6 +185,11 @@
         </main>
     </div>
     <footer>This is footer</footer>
+    <script src="{{asset('dist/libs/nouislider/dist/nouislider.min.js')}}" defer></script>
+    <script src="{{asset('dist/libs/litepicker/dist/litepicker.js')}}" defer></script>
+    <script src="{{asset('dist/libs/tom-select/dist/js/tom-select.base.min.js')}}" defer></script>
+    <script src="{{asset('dist/js/tabler.min.js')}}" defer></script>
+    <script src="{{asset('dist/js/demo.min.js')}}" defer></script>
 </body>
 
 </html>
