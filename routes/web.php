2c9e97883caa85385f668dfddffc923dab79c6f0
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-23 14:20:39 +0530

    feat: add field add brand route in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
@@ -11,38 +10,43 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Route::group(["middleware" => AdminMiddleware::class], function () {
+Route::group(['middleware' => AdminMiddleware::class], function () {
 
     Route::get('admin', function () {
         return view('admin');
-    })->name("admin");
+    })->name('admin');
 
-    Route::group(["prefix" => "/product"], function () {
+    Route::group(['prefix' => '/product'], function () {
         Route::get('category', [CategoryController::class, 'index'])->name('category');
 
         Route::get('add', [ProductController::class, 'index'])->name('product.add');
 
-        Route::get('view', [ProductController::class, "admindisplay"])->name('product.view');
+        Route::get('view', [ProductController::class, 'admindisplay'])->name('product.view');
 
-        Route::post('update/{id}', [ProductController::class, "update"])->name('product.update');
+        Route::post('update/{id}', [ProductController::class, 'update'])->name('product.update');
 
-        Route::delete('delete/{id}', [ProductController::class, "delete"])->name('product.delete');
+        Route::delete('delete/{id}', [ProductController::class, 'delete'])->name('product.delete');
+
+        Route::get('brand', function () {
+
+            return view('brand');
+        })->name('brand');
     });
 });
 
-Route::get("/child-category/{id}", [ProductController::class, "child_category"])->name("child-category");
+Route::get('/child-category/{id}', [ProductController::class, 'child_category'])->name('child-category');
 
-Route::get("login", function () {
-    return view("login");
-})->name("login");
+Route::get('login', function () {
+    return view('login');
+})->name('login');
 
-Route::get("logout", [AuthController::class, "logout"])->name("logout");
+Route::get('logout', [AuthController::class, 'logout'])->name('logout');
 
+Route::post('category/add', [CategoryController::class, 'store'])->name('category.add');
+Route::post('category/child/add', [CategoryController::class, 'storechild'])->name('category.child.add');
+Route::post('brand/add', [CategoryController::class, 'storechild'])->name('brand.add');
+Route::post('/product/add', [ProductController::class, 'store'])->name('product.add');
+Route::post('login', [AuthController::class, 'login'])->name('login');
 
-Route::post("category/add", [CategoryController::class, "store"])->name("category.add");
-Route::post("category/child/add", [CategoryController::class, "storechild"])->name("category.child.add");
-Route::post("/product/add", [ProductController::class, "store"])->name("product.add");
-Route::post("login", [AuthController::class, "login"])->name("login");
-
-Route::get("/", [ProductController::class, "display"])->name("product");
-Route::get("product/{id}", [ProductController::class, "specific"])->name("product.specific");
+Route::get('/', [ProductController::class, 'display'])->name('product');
+Route::get('product/{id}', [ProductController::class, 'specific'])->name('product.specific');
