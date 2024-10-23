8e0364fafa7b36930fc12852fcee929f0832b337
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-23 12:58:31 +0530

    fix: update rules for product store

--- app/Http/Requests/ProductAddRequest.php
+++ app/Http/Requests/ProductAddRequest.php
@@ -22,31 +22,32 @@ class ProductAddRequest extends FormRequest
     public function rules(): array
     {
         return [
-            "name" => "required",
-            "description" => "required",
-            "price" => "required|numeric|regex:/^\d+(\.\d{1,2})?$/",  // Corrected the price rule
-            "stock" => "required|integer|min:1",
-            "category_id" => "required|exists:categories,id",
-            "brand_id" => "required|exists:brands,id",
-            "sku" => "required",
-            "weight" => "required",
+            'name' => 'required',
+            'description' => 'required',
+            'price' => "required|numeric|regex:/^\d+(\.\d{1,2})?$/",  // Corrected the price rule
+            'stock' => 'required|integer|min:1',
+            'category_id' => 'required|exists:categories,id',
+            'brand_id' => 'required|exists:brands,id',
+            'sku' => 'required',
+            'weight' => 'required',
+            'slug' => 'required|unique:products,slug',
         ];
     }
 
     public function messages(): array
     {
         return [
-            "name.required" => "Product name is required",
-            "description.required" => "Product description is required",
-            "price.required" => "Product price is required",
-            "price.numeric" => "Product price must be a valid number",
-            "price.regex" => "Product price must have up to 2 decimal places",
-            "stock.required" => "Product stock is required",
-            "category_id.required" => "Product category is required",
-            "category_id.exists" => "The selected category does not exist",
-            "added_by.required" => "Product added by is required",
-            "brand_id.required" => "Brand is required",
-            "brand_id.exists" => "Brand does not exist",
+            'name.required' => 'Product name is required',
+            'description.required' => 'Product description is required',
+            'price.required' => 'Product price is required',
+            'price.numeric' => 'Product price must be a valid number',
+            'price.regex' => 'Product price must have up to 2 decimal places',
+            'stock.required' => 'Product stock is required',
+            'category_id.required' => 'Product category is required',
+            'category_id.exists' => 'The selected category does not exist',
+            'added_by.required' => 'Product added by is required',
+            'brand_id.required' => 'Brand is required',
+            'brand_id.exists' => 'Brand does not exist',
         ];
     }
 }
