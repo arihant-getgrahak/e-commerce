17581e2e8d46fc151e3b8f88f576b5935d9aa5aa
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-24 23:01:59 +0530

    fix: update cost_price field name

--- database/migrations/2024_10_24_171552_add_col_to_products.php
+++ database/migrations/2024_10_24_171552_add_col_to_products.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->string('cost-price')->nullable();
+            $table->string('cost_price')->nullable();
         });
     }
 
