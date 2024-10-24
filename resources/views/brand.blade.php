a060ae2f0d6d86bb427b9154a9dba6bfa29ee2c0
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-24 13:22:42 +0530

    feat: add delete brand modal in category table

--- resources/views/brand.blade.php
+++ resources/views/brand.blade.php
@@ -22,6 +22,84 @@
             Add Brand
         </button>
     </form>
+
+    <div class="card">
+        <div class="card-body">
+            <h1>All Category</h1>
+            <div id="table-default" class="table-responsive">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th><button class="table-sort" data-sort="sort-name">Id</button></th>
+                            <th><button class="table-sort" data-sort="sort-city">Name</button></th>
+                            <th><button class="table-sort" data-sort="sort-type">Buttons</button></th>
+                        </tr>
+                    </thead>
+                    <tbody class="table-tbody">
+                        @foreach ($brand as $c)
+                            <tr>
+                                <td class="sort-name">{{$c["id"]}}</td>
+                                <td class="sort-city">{{$c["name"]}}</td>
+                                <td class="sort-type">
+                                    <button class="btn btn-primary btn-sm btn-update" data-bs-toggle="modal"
+                                        data-bs-target="#modal-team" data-id="{{ $c["id"] }}"
+                                        data-name="{{ $c["name"] }}">Edit</button>
+                                    <button class="btn btn-danger btn-sm" data-bs-toggle="modal"
+                                        data-bs-target="#modal-danger">Delete</button>
+                                </td>
+                            </tr>
+                            <div class="modal modal-blur fade" id="modal-danger" tabindex="-1" role="dialog"
+                                aria-hidden="true">
+                                <div class="modal-dialog modal-sm modal-dialog-centered" role="document">
+                                    <div class="modal-content">
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                            aria-label="Close"></button>
+                                        <div class="modal-status bg-danger"></div>
+                                        <div class="modal-body text-center py-4">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="icon mb-2 text-danger icon-lg"
+                                                width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
+                                                stroke="currentColor" fill="none" stroke-linecap="round"
+                                                stroke-linejoin="round">
+                                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                <path
+                                                    d="M10.24 3.957l-8.422 14.06a1.989 1.989 0 0 0 1.7 2.983h16.845a1.989 1.989 0 0 0 1.7 -2.983l-8.423 -14.06a1.989 1.989 0 0 0 -3.4 0z" />
+                                                <path d="M12 9v4" />
+                                                <path d="M12 17h.01" />
+                                            </svg>
+                                            <h3>Are you sure?</h3>
+                                            <div class="text-secondary">Do you really want to delete this product? This
+                                                action cannot be
+                                                undone.
+                                            </div>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <div class="w-100">
+                                                <div class="row">
+                                                    <div class="col"><a href="#" class="btn w-100"
+                                                            data-bs-dismiss="modal">Cancel</a></div>
+                                                    <div class="col">
+                                                        <form id="delete-form"
+                                                            action="{{ route('category.delete', $c['id']) }}" method="POST"
+                                                            class="w-100">
+                                                            @csrf
+                                                            @method('DELETE')
+                                                            <button type="submit" class="btn btn-danger w-100">Delete
+                                                                product</button>
+                                                        </form>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </div>
 
 
