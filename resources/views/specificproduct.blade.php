6ed4686e214e54196cc0abd1e009421f77a48597
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-25 10:59:43 +0530

    feat: add matching product suggestion box

--- resources/views/specificproduct.blade.php
+++ resources/views/specificproduct.blade.php
@@ -418,6 +418,255 @@
                 </div>
             </div>
         </section>
+        <!-- ======================= Similar Products Start ============================ -->
+        <section class="middle pt-0">
+            <div class="container">
+
+                <div class="row justify-content-center">
+                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                        <div class="sec_title position-relative text-center">
+                            <h2 class="off_title">Similar Products</h2>
+                            <h3 class="ft-bold pt-3">Matching Producta</h3>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                        <div class="slide_items">
+
+                            <!-- single Item -->
+                            <div class="single_itesm">
+                                <div class="product_grid card b-0 mb-0">
+                                    <div
+                                        class="badge bg-success text-white position-absolute ft-regular ab-left text-upper">
+                                        Sale</div>
+                                    <button class="snackbar-wishlist btn btn_love position-absolute ab-right"><i
+                                            class="far fa-heart"></i></button>
+                                    <div class="card-body p-0">
+                                        <div class="shop_thumb position-relative">
+                                            <a class="card-img-top d-block overflow-hidden" href="shop-single-v1.html"><img
+                                                    class="card-img-top" src="assets/img/product/8.jpg" alt="..."></a>
+                                            <div
+                                                class="product-hover-overlay bg-dark d-flex align-items-center justify-content-center">
+                                                <div class="edlio"><a href="#" data-toggle="modal" data-target="#quickview"
+                                                        class="text-white fs-sm ft-medium"><i
+                                                            class="fas fa-eye mr-1"></i>Quick View</a></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="card-footer b-0 p-3 pb-0 d-flex align-items-start justify-content-center">
+                                        <div class="text-left">
+                                            <div class="text-center">
+                                                <h5 class="fw-bolder fs-md mb-0 lh-1 mb-1"><a
+                                                        href="shop-single-v1.html">Half Running Set</a></h5>
+                                                <div class="elis_rty"><span class="ft-bold fs-md text-dark">$119.00</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!-- single Item -->
+                            <div class="single_itesm">
+                                <div class="product_grid card b-0 mb-0">
+                                    <div class="badge bg-info text-white position-absolute ft-regular ab-left text-upper">
+                                        New</div>
+                                    <button class="snackbar-wishlist btn btn_love position-absolute ab-right"><i
+                                            class="far fa-heart"></i></button>
+                                    <div class="card-body p-0">
+                                        <div class="shop_thumb position-relative">
+                                            <a class="card-img-top d-block overflow-hidden" href="shop-single-v1.html"><img
+                                                    class="card-img-top" src="assets/img/product/9.jpg" alt="..."></a>
+                                            <div
+                                                class="product-hover-overlay bg-dark d-flex align-items-center justify-content-center">
+                                                <div class="edlio"><a href="#" data-toggle="modal" data-target="#quickview"
+                                                        class="text-white fs-sm ft-medium"><i
+                                                            class="fas fa-eye mr-1"></i>Quick View</a></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="card-footer b-0 p-3 pb-0 d-flex align-items-start justify-content-center">
+                                        <div class="text-left">
+                                            <div class="text-center">
+                                                <h5 class="fw-bolder fs-md mb-0 lh-1 mb-1"><a
+                                                        href="shop-single-v1.html">Formal Men Lowers</a></h5>
+                                                <div class="elis_rty"><span
+                                                        class="text-muted ft-medium line-through mr-2">$129.00</span><span
+                                                        class="ft-bold theme-cl fs-md">$79.00</span></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!-- single Item -->
+                            <div class="single_itesm">
+                                <div class="product_grid card b-0 mb-0">
+                                    <button class="snackbar-wishlist btn btn_love position-absolute ab-right"><i
+                                            class="far fa-heart"></i></button>
+                                    <div class="card-body p-0">
+                                        <div class="shop_thumb position-relative">
+                                            <a class="card-img-top d-block overflow-hidden" href="shop-single-v1.html"><img
+                                                    class="card-img-top" src="assets/img/product/10.jpg" alt="..."></a>
+                                            <div
+                                                class="product-hover-overlay bg-dark d-flex align-items-center justify-content-center">
+                                                <div class="edlio"><a href="#" data-toggle="modal" data-target="#quickview"
+                                                        class="text-white fs-sm ft-medium"><i
+                                                            class="fas fa-eye mr-1"></i>Quick View</a></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="card-footer b-0 p-3 pb-0 d-flex align-items-start justify-content-center">
+                                        <div class="text-left">
+                                            <div class="text-center">
+                                                <h5 class="fw-bolder fs-md mb-0 lh-1 mb-1"><a
+                                                        href="shop-single-v1.html">Half Running Suit</a></h5>
+                                                <div class="elis_rty"><span class="ft-bold fs-md text-dark">$80.00</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!-- single Item -->
+                            <div class="single_itesm">
+                                <div class="product_grid card b-0 mb-0">
+                                    <div
+                                        class="badge bg-warning text-white position-absolute ft-regular ab-left text-upper">
+                                        Hot</div>
+                                    <button class="snackbar-wishlist btn btn_love position-absolute ab-right"><i
+                                            class="far fa-heart"></i></button>
+                                    <div class="card-body p-0">
+                                        <div class="shop_thumb position-relative">
+                                            <a class="card-img-top d-block overflow-hidden" href="shop-single-v1.html"><img
+                                                    class="card-img-top" src="assets/img/product/11.jpg" alt="..."></a>
+                                            <div
+                                                class="product-hover-overlay bg-dark d-flex align-items-center justify-content-center">
+                                                <div class="edlio"><a href="#" data-toggle="modal" data-target="#quickview"
+                                                        class="text-white fs-sm ft-medium"><i
+                                                            class="fas fa-eye mr-1"></i>Quick View</a></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="card-footer b-0 p-3 pb-0 d-flex align-items-start justify-content-center">
+                                        <div class="text-left">
+                                            <div class="text-center">
+                                                <h5 class="fw-bolder fs-md mb-0 lh-1 mb-1"><a
+                                                        href="shop-single-v1.html">Half Fancy Lady Dress</a></h5>
+                                                <div class="elis_rty"><span
+                                                        class="text-muted ft-medium line-through mr-2">$149.00</span><span
+                                                        class="ft-bold theme-cl fs-md">$110.00</span></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!-- single Item -->
+                            <div class="single_itesm">
+                                <div class="product_grid card b-0 mb-0">
+                                    <button class="snackbar-wishlist btn btn_love position-absolute ab-right"><i
+                                            class="far fa-heart"></i></button>
+                                    <div class="card-body p-0">
+                                        <div class="shop_thumb position-relative">
+                                            <a class="card-img-top d-block overflow-hidden" href="shop-single-v1.html"><img
+                                                    class="card-img-top" src="assets/img/product/12.jpg" alt="..."></a>
+                                            <div
+                                                class="product-hover-overlay bg-dark d-flex align-items-center justify-content-center">
+                                                <div class="edlio"><a href="#" data-toggle="modal" data-target="#quickview"
+                                                        class="text-white fs-sm ft-medium"><i
+                                                            class="fas fa-eye mr-1"></i>Quick View</a></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="card-footer b-0 p-3 pb-0 d-flex align-items-start justify-content-center">
+                                        <div class="text-left">
+                                            <div class="text-center">
+                                                <h5 class="fw-bolder fs-md mb-0 lh-1 mb-1"><a
+                                                        href="shop-single-v1.html">Flix Flox Jeans</a></h5>
+                                                <div class="elis_rty"><span
+                                                        class="text-muted ft-medium line-through mr-2">$90.00</span><span
+                                                        class="ft-bold theme-cl fs-md">$49.00</span></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!-- single Item -->
+                            <div class="single_itesm">
+                                <div class="product_grid card b-0 mb-0">
+                                    <div class="badge bg-danger text-white position-absolute ft-regular ab-left text-upper">
+                                        Hot</div>
+                                    <button class="snackbar-wishlist btn btn_love position-absolute ab-right"><i
+                                            class="far fa-heart"></i></button>
+                                    <div class="card-body p-0">
+                                        <div class="shop_thumb position-relative">
+                                            <a class="card-img-top d-block overflow-hidden" href="shop-single-v1.html"><img
+                                                    class="card-img-top" src="assets/img/product/13.jpg" alt="..."></a>
+                                            <div
+                                                class="product-hover-overlay bg-dark d-flex align-items-center justify-content-center">
+                                                <div class="edlio"><a href="#" data-toggle="modal" data-target="#quickview"
+                                                        class="text-white fs-sm ft-medium"><i
+                                                            class="fas fa-eye mr-1"></i>Quick View</a></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="card-footer b-0 p-3 pb-0 d-flex align-items-start justify-content-center">
+                                        <div class="text-left">
+                                            <div class="text-center">
+                                                <h5 class="fw-bolder fs-md mb-0 lh-1 mb-1"><a
+                                                        href="shop-single-v1.html">Fancy Salwar Suits</a></h5>
+                                                <div class="elis_rty"><span class="ft-bold fs-md text-dark">$114.00</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!-- single Item -->
+                            <div class="single_itesm">
+                                <div class="product_grid card b-0 mb-0">
+                                    <div
+                                        class="badge bg-success text-white position-absolute ft-regular ab-left text-upper">
+                                        Sale</div>
+                                    <button class="snackbar-wishlist btn btn_love position-absolute ab-right"><i
+                                            class="far fa-heart"></i></button>
+                                    <div class="card-body p-0">
+                                        <div class="shop_thumb position-relative">
+                                            <a class="card-img-top d-block overflow-hidden" href="shop-single-v1.html"><img
+                                                    class="card-img-top" src="assets/img/product/14.jpg" alt="..."></a>
+                                            <div
+                                                class="product-hover-overlay bg-dark d-flex align-items-center justify-content-center">
+                                                <div class="edlio"><a href="#" data-toggle="modal" data-target="#quickview"
+                                                        class="text-white fs-sm ft-medium"><i
+                                                            class="fas fa-eye mr-1"></i>Quick View</a></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="card-footer b-0 p-3 pb-0 d-flex align-items-start justify-content-center">
+                                        <div class="text-left">
+                                            <div class="text-center">
+                                                <h5 class="fw-bolder fs-md mb-0 lh-1 mb-1"><a
+                                                        href="shop-single-v1.html">Collot Full Dress</a></h5>
+                                                <div class="elis_rty"><span
+                                                        class="ft-bold theme-cl fs-md text-dark">$120.00</span></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </section>
         </div>
 
     @endif
