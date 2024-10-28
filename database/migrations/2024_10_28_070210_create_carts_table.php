779390af48dd391134ac8235ad49b798d7e2d05f
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-28 14:10:16 +0530

    fix: add price field to cart table

--- database/migrations/2024_10_28_070210_create_carts_table.php
+++ database/migrations/2024_10_28_070210_create_carts_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->foreignId('user_id');
             $table->foreignId('product_id');
             $table->integer('quantity');
+            $table->float('price');
             $table->timestamps();
         });
     }
