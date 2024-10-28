6a471d73b70f7b32ee7a03769bf706c2e23cd3c6
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-28 10:48:31 +0530

    feat: add breadcrumb in layout frontend

--- resources/views/layout/frontend.blade.php
+++ resources/views/layout/frontend.blade.php
@@ -220,6 +220,21 @@
         </div>
         <!-- End Navigation -->
         <div class="clearfix"></div>
+        <div class="gray py-3">
+            <div class="container">
+                <div class="row">
+                    <div class="colxl-12 col-lg-12 col-md-12">
+                        <nav aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                <li class="breadcrumb-item"><a href="#">Pages</a></li>
+                                <li class="breadcrumb-item active" aria-current="page">Login</li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
         @yield('section')
         <footer class="dark-footer skin-dark-footer style-2">
             <div class="footer-middle">
