e03d78797b0be5dfcea91eebce68295fe843b370
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-28 14:21:02 +0530

    fix: update cartcontroller to view cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -11,7 +11,9 @@ class CartController extends Controller
 {
     public function index()
     {
-        return view('cart');
+        $cart = Cart::where('user_id', auth()->user()->id)->get();
+
+        return view('cart')->with('cart', $cart);
     }
 
     public function store(CartStoreRequest $request)
