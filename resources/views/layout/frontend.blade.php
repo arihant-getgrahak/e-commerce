0c99bdb3d8fa992ecf2f53ca14709fdbec9557a6
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-28 10:54:32 +0530

    feat: add login modal in layout/frontend

--- resources/views/layout/frontend.blade.php
+++ resources/views/layout/frontend.blade.php
@@ -344,6 +344,57 @@
                 </div>
             </div>
         </footer>
+
+        <!-- Log In Modal -->
+        <div class="modal fade" id="login" tabindex="-1" role="dialog" aria-labelledby="loginmodal" aria-hidden="true">
+            <div class="modal-dialog modal-xl login-pop-form" role="document">
+                <div class="modal-content" id="loginmodal">
+                    <div class="modal-headers">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span class="ti-close"></span>
+                        </button>
+                    </div>
+
+                    <div class="modal-body p-5">
+                        <div class="text-center mb-4">
+                            <h2 class="m-0 ft-regular">Login</h2>
+                        </div>
+
+                        <form action="{{ route('login') }}" method="post" autocomplete="off">
+                            @csrf
+                            <div class="form-group">
+                                <label>Email</label>
+                                <input type="email" class="form-control" placeholder="your@email.com" autocomplete="off"
+                                    name="email" id="email" value="{{ old('email') }}">
+                                @error('email')
+                                    <p class="text-red-500">{{ $message }}</p>
+                                @enderror
+                            </div>
+
+                            <div class="form-group">
+                                <label>Password</label>
+                                <input type="password" class="form-control" placeholder="Password*" name="password"
+                                    id="password">
+                                @error('password')
+                                    <p class="text-red-500">{{ $message }}</p>
+                                @enderror
+                            </div>
+
+                            <div class="form-group">
+                                <button type="submit"
+                                    class="btn btn-md full-width bg-dark text-light fs-md ft-medium">Login</button>
+                            </div>
+
+                            <div class="form-group text-center mb-0">
+                                <p class="extra">Not a member?<a href="#et-register-wrap" class="text-dark">
+                                        Register</a></p>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Modal -->
     </div>
 
     <script src="{{asset('assets/js/jquery.min.js')}}"></script>
