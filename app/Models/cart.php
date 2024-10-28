768dee1f268651c5e1fb0ff1b666651ae88dcba1
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-28 14:02:51 +0530

    feat: add fillable and relation in cart model

--- app/Models/cart.php
+++ app/Models/cart.php
@@ -4,7 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class cart extends Model
+class Cart extends Model
 {
-    //
+    protected $fillable = ['user_id', 'product_id', 'quantity'];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class, 'id', 'product_id');
+    }
+
+    public function users()
+    {
+        return $this->belongsTo(User::class, 'id', 'user_id');
+    }
+
+    protected function casts(): array
+    {
+        return [
+            'created_at' => 'datetime:Y-m-d',
+            'updated_at' => 'datetime:Y-m-d',
+        ];
+    }
 }
