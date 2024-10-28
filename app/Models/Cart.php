3dfde6ed558d8d5a89c3993b7cc3718934276e4f
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-28 14:10:42 +0530

    fix: add price field in cart modal fillable

---
+++ app/Models/Cart.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cart extends Model
+{
+    protected $fillable = ['user_id', 'product_id', 'quantity', 'price'];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class, 'id', 'product_id');
+    }
+
+    public function users()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    protected function casts(): array
+    {
+        return [
+            'created_at' => 'datetime:Y-m-d',
+            'updated_at' => 'datetime:Y-m-d',
+        ];
+    }
+}
