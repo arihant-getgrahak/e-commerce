58f9df210c0dc7fdfec4d98b966562c686c51cec
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-28 14:53:19 +0530

    feat: add check in cart view

--- resources/views/layout/frontend.blade.php
+++ resources/views/layout/frontend.blade.php
@@ -11,6 +11,9 @@
         .text-red-500 {
             color: red;
         }
+        .underline{
+            text-decoration: underline;
+        }
     </style>
 </head>
 
@@ -408,78 +411,81 @@
                 <button onclick="closeCart()" class="close_slide"><i class="ti-close"></i></button>
             </div>
             <div class="right-ch-sideBar">
+                @if(Auth::check())
 
-                <div class="cart_select_items py-2">
-                    <!-- Single Item -->
-                    <div class="d-flex align-items-center justify-content-between br-bottom px-3 py-3">
-                        <div class="cart_single d-flex align-items-center">
-                            <div class="cart_selected_single_thumb">
-                                <a href="#"><img src="assets/img/product/4.jpg" width="60" class="img-fluid"
-                                        alt="" /></a>
+                        <div class="cart_select_items py-2">
+                            <!-- Single Item -->
+                            <div class="d-flex align-items-center justify-content-between br-bottom px-3 py-3">
+                                <div class="cart_single d-flex align-items-center">
+                                    <div class="cart_selected_single_thumb">
+                                        <a href="#"><img src="assets/img/product/4.jpg" width="60" class="img-fluid"
+                                                alt="" /></a>
+                                    </div>
+                                    <div class="cart_single_caption pl-2">
+                                        <h4 class="product_title fs-sm ft-medium mb-0 lh-1">Women Striped Shirt Dress</h4>
+                                        <p class="mb-2"><span class="text-dark ft-medium small">36</span>, <span
+                                                class="text-dark small">Red</span></p>
+                                        <h4 class="fs-md ft-medium mb-0 lh-1">$129</h4>
+                                    </div>
+                                </div>
+                                <div class="fls_last"><button class="close_slide gray"><i class="ti-close"></i></button></div>
                             </div>
-                            <div class="cart_single_caption pl-2">
-                                <h4 class="product_title fs-sm ft-medium mb-0 lh-1">Women Striped Shirt Dress</h4>
-                                <p class="mb-2"><span class="text-dark ft-medium small">36</span>, <span
-                                        class="text-dark small">Red</span></p>
-                                <h4 class="fs-md ft-medium mb-0 lh-1">$129</h4>
+
+                            <!-- Single Item -->
+                            <div class="d-flex align-items-center justify-content-between br-bottom px-3 py-3">
+                                <div class="cart_single d-flex align-items-center">
+                                    <div class="cart_selected_single_thumb">
+                                        <a href="#"><img src="assets/img/product/7.jpg" width="60" class="img-fluid"
+                                                alt="" /></a>
+                                    </div>
+                                    <div class="cart_single_caption pl-2">
+                                        <h4 class="product_title fs-sm ft-medium mb-0 lh-1">Girls Floral Print Jumpsuit</h4>
+                                        <p class="mb-2"><span class="text-dark ft-medium small">36</span>, <span
+                                                class="text-dark small">Red</span></p>
+                                        <h4 class="fs-md ft-medium mb-0 lh-1">$129</h4>
+                                    </div>
+                                </div>
+                                <div class="fls_last"><button class="close_slide gray"><i class="ti-close"></i></button></div>
+                            </div>
+
+                            <!-- Single Item -->
+                            <div class="d-flex align-items-center justify-content-between px-3 py-3">
+                                <div class="cart_single d-flex align-items-center">
+                                    <div class="cart_selected_single_thumb">
+                                        <a href="#"><img src="assets/img/product/8.jpg" width="60" class="img-fluid"
+                                                alt="" /></a>
+                                    </div>
+                                    <div class="cart_single_caption pl-2">
+                                        <h4 class="product_title fs-sm ft-medium mb-0 lh-1">Girls Solid A-Line Dress</h4>
+                                        <p class="mb-2"><span class="text-dark ft-medium small">30</span>, <span
+                                                class="text-dark small">Blue</span></p>
+                                        <h4 class="fs-md ft-medium mb-0 lh-1">$100</h4>
+                                    </div>
+                                </div>
+                                <div class="fls_last"><button class="close_slide gray"><i class="ti-close"></i></button></div>
+                            </div>
+
+                        </div>
+
+                        <div class="d-flex align-items-center justify-content-between br-top br-bottom px-3 py-3">
+                            <h6 class="mb-0">Subtotal</h6>
+                            <h3 class="mb-0 ft-medium">$1023</h3>
+                        </div>
+
+                        <div class="cart_action px-3 py-3">
+                            <div class="form-group">
+                                <button type="button" class="btn d-block full-width btn-dark">Checkout Now</button>
+                            </div>
+                            <div class="form-group">
+                                <button type="button" class="btn d-block full-width btn-dark-light">Edit or View</button>
                             </div>
                         </div>
-                        <div class="fls_last"><button class="close_slide gray"><i class="ti-close"></i></button></div>
+
                     </div>
-
-                    <!-- Single Item -->
-                    <div class="d-flex align-items-center justify-content-between br-bottom px-3 py-3">
-                        <div class="cart_single d-flex align-items-center">
-                            <div class="cart_selected_single_thumb">
-                                <a href="#"><img src="assets/img/product/7.jpg" width="60" class="img-fluid"
-                                        alt="" /></a>
-                            </div>
-                            <div class="cart_single_caption pl-2">
-                                <h4 class="product_title fs-sm ft-medium mb-0 lh-1">Girls Floral Print Jumpsuit</h4>
-                                <p class="mb-2"><span class="text-dark ft-medium small">36</span>, <span
-                                        class="text-dark small">Red</span></p>
-                                <h4 class="fs-md ft-medium mb-0 lh-1">$129</h4>
-                            </div>
-                        </div>
-                        <div class="fls_last"><button class="close_slide gray"><i class="ti-close"></i></button></div>
+                @else
                     </div>
-
-                    <!-- Single Item -->
-                    <div class="d-flex align-items-center justify-content-between px-3 py-3">
-                        <div class="cart_single d-flex align-items-center">
-                            <div class="cart_selected_single_thumb">
-                                <a href="#"><img src="assets/img/product/8.jpg" width="60" class="img-fluid"
-                                        alt="" /></a>
-                            </div>
-                            <div class="cart_single_caption pl-2">
-                                <h4 class="product_title fs-sm ft-medium mb-0 lh-1">Girls Solid A-Line Dress</h4>
-                                <p class="mb-2"><span class="text-dark ft-medium small">30</span>, <span
-                                        class="text-dark small">Blue</span></p>
-                                <h4 class="fs-md ft-medium mb-0 lh-1">$100</h4>
-                            </div>
-                        </div>
-                        <div class="fls_last"><button class="close_slide gray"><i class="ti-close"></i></button></div>
-                    </div>
-
-                </div>
-
-                <div class="d-flex align-items-center justify-content-between br-top br-bottom px-3 py-3">
-                    <h6 class="mb-0">Subtotal</h6>
-                    <h3 class="mb-0 ft-medium">$1023</h3>
-                </div>
-
-                <div class="cart_action px-3 py-3">
-                    <div class="form-group">
-                        <button type="button" class="btn d-block full-width btn-dark">Checkout Now</button>
-                    </div>
-                    <div class="form-group">
-                        <button type="button" class="btn d-block full-width btn-dark-light">Edit or View</button>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    </div>
+                    <p>Please <a class="underline" href="{{route('login')}}">login</a> to view cart</p>
+                @endif
     </div>
 
     <script src="{{asset('assets/js/jquery.min.js')}}"></script>
