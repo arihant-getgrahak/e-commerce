124b91e9654083ca519a5c59d15da780f3461708
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-30 10:44:13 +0530

    fix: add fillable in orderAddress  modal

--- app/Models/OrderAdress.php
+++ app/Models/OrderAdress.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderAdress extends Model
 {
-    //
+    protected $fillable = [
+        'order_id',
+        'user_id',
+        'name',
+        'email',
+        'address',
+        'city',
+        'state',
+        'country',
+        'pincode',
+        'phone',
+    ];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class, 'order_id', 'id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
 }
