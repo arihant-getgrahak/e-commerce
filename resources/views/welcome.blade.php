2e8a17fd8e504deeaf0cc42663c17011a3aea529
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-30 15:18:45 +0530

    feat: update welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -914,31 +914,5 @@
         });
     });
 </script>
-<script>
-    async function addToCart(this) {
-        console.log(this);
-        return
-        const quantity = 1;
-        const res = await fetch("{{route("cart.add")}}", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "X-CSRF-TOKEN": "{{ csrf_token() }}"
-            },
-            body: JSON.stringify({
-                product_id: productId[0].id,
-                quantity: quantity,
-                price: productId[0].price
-            }),
-        })
 
-        const data = await res.json()
-        if (!data.status) {
-            alert(data.message)
-        }
-        else {
-            alert(data.message)
-        }
-    }
-</script>
 @endsection
