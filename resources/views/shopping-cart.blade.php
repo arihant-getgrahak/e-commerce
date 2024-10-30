6fd9671f53478481dee99329b9098572f1306745
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-30 14:50:41 +0530

    fix: update shopping-cart view add checkout url

--- resources/views/shopping-cart.blade.php
+++ resources/views/shopping-cart.blade.php
@@ -93,7 +93,7 @@
                             </div>
                         </div>
 
-                        <a class="btn btn-block btn-dark mb-3" href="checkout.html">Proceed to Checkout</a>
+                        <a class="btn btn-block btn-dark mb-3" href="{{route("checkout")}}">Proceed to Checkout</a>
 
                         <a class="btn-link text-dark ft-medium" href="shop.html">
                             <i class="ti-back-left mr-2"></i> Continue Shopping
