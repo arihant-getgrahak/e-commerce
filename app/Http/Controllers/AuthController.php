e1782bc559aa487dccfd647488bdf0bb5d6f6180
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-11-05 16:37:12 +0530

    feat: add google login func

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -71,5 +71,16 @@ class AuthController extends Controller
         return Socialite::driver('google')->redirect();
     }
 
-    public function handleGoogleCallback() {}
+    public function handleGoogleCallback()
+    {
+        $googleUser = Socialite::driver('google')->stateless()->user();
+        $user = User::where('email', $googleUser->email)->first();
+        if (! $user) {
+            $user = User::create(['name' => $googleUser->name, 'email' => $googleUser->email, 'password' => \Hash::make(rand(100000, 999999))]);
+        }
+
+        Auth::login($user);
+
+        return redirect('/');
+    }
 }
