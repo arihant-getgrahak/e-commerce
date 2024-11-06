ae8862813d75004eb6ad9618437e9026875945df
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-11-06 21:34:58 +0530

    feat: add sweet alert in addcategory view

--- resources/views/addcategroy.blade.php
+++ resources/views/addcategroy.blade.php
@@ -155,11 +155,31 @@
     </div>
 </main>
 <script>
+    const arihant = document.querySelector('#alert');
     if ("{{session('success')}}") {
-        alert("{{session('success')}}")
+        arihant.innerHTML = `
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+        ${{{ session('success') }}}
+                                              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+												<span aria-hidden="true">&times;</span>
+											  </button>
+											</div>
+                                            `
+        window.scrollTo(0, 0);
     }
-
-
+    if ("{{session('error')}}") {
+        arihant.innerHTML = `
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        ${{{ session('error') }}}
+                                              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+												<span aria-hidden="true">&times;</span>
+											  </button>
+											</div>
+                                            `
+        window.scrollTo(0, 0);
+    }
+</script>
+<script>
     document.addEventListener('DOMContentLoaded', function () {
         const updateButtons = document.querySelectorAll('.btn-update');
         const updateForm = document.getElementById("productForm");
