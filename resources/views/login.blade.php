69a497dcb344784a52f62abb89936bbd3c487d05
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-11-06 21:33:08 +0530

    feat: add sweet alert in login view

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -81,11 +81,28 @@
     <script src="{{asset('dist/js/tabler.min.js')}}" defer></script>
     <script src="{{asset('dist/js/demo.min.js')}}" defer></script>
     <script>
-        if ("{{ session('success') }}") {
-            alert("{{ session('success') }}");
+        const arihant = document.querySelector('#alert');
+        if ("{{session('success')}}") {
+            arihant.innerHTML = `
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+        ${{{ session('success') }}}
+                                              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+												<span aria-hidden="true">&times;</span>
+											  </button>
+											</div>
+                                            `
+            window.scrollTo(0, 0);
         }
-        if ("{{ session('error') }}") {
-            alert("{{ session('error') }}");
+        if ("{{session('error')}}") {
+            arihant.innerHTML = `
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        ${{{ session('error') }}}
+                                              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+												<span aria-hidden="true">&times;</span>
+											  </button>
+											</div>
+                                            `
+            window.scrollTo(0, 0);
         }
     </script>
 </body>
