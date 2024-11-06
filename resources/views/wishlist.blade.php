2eb726f30d8e01d73a49337f741a5f5b6b7eb67d
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-11-06 21:35:51 +0530

    feat: add sweet alert in wishlist view

--- resources/views/wishlist.blade.php
+++ resources/views/wishlist.blade.php
@@ -84,8 +84,15 @@
                 window.location.reload();
             }
         } catch (error) {
-            console.error("Error deleting wishlist item:", error);
-            alert("An error occurred. Please try again.");
+            arihant.innerHTML = `
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        ${error}
+                                              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+												<span aria-hidden="true">&times;</span>
+											  </button>
+											</div>
+                                            `
+            window.scrollTo(0, 0);
         }
     }
 </script>
