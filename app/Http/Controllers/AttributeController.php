f1ef039d999993737646339034c303d8f5b8d76f
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-11-07 16:54:46 +0530

    feat: add index func in attribute controller

--- app/Http/Controllers/AttributeController.php
+++ app/Http/Controllers/AttributeController.php
@@ -2,7 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Attributes;
+
 class AttributeController extends Controller
 {
-    //
+    public function index()
+    {
+        $attributes = Attributes::with('values')->get();
+
+        return view('attribute', compact('attributes'));
+    }
 }
