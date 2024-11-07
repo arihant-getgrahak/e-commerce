8c3f5f3cd71db7b137529f519619b4c2aa17b543
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-11-07 16:56:54 +0530

    feat: create func to store attribute in db

--- app/Http/Controllers/AttributeController.php
+++ app/Http/Controllers/AttributeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Attributes;
+use Request;
+use Validator;
 
 class AttributeController extends Controller
 {
@@ -11,5 +13,26 @@ class AttributeController extends Controller
         $attributes = Attributes::with('values')->get();
 
         return view('attribute', compact('attributes'));
+    }
+
+    public function store(Request $request)
+    {
+        $validation = Validator::make($request->all(), [
+            'name' => 'required',
+        ]);
+
+        if ($validation->fails()) {
+            return back()->with('errors', $validation->errors());
+        }
+
+        $attribute = Attributes::create([
+            'name' => $request->name,
+        ]);
+
+        if (! $attribute) {
+            return back()->with('error', 'Attribute not created');
+        }
+
+        return back()->with('success', 'Attribute created successfully');
     }
 }
