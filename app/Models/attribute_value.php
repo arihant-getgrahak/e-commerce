939acfa9cca5a1b74fe2d2770d556ea7c0a1c57c
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-11-07 16:17:28 +0530

    feat: add field in attribute value model class

--- app/Models/attribute_value.php
+++ app/Models/attribute_value.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class attribute_value extends Model
 {
-    //
+    protected $fillable = ['value', 'attribute_id', 'color_code'];
+
+    public function attribute()
+    {
+        $this->belongsTo(Attributes::class, 'attribute_id');
+    }
 }
