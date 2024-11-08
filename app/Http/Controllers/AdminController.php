1a39bd5b6e83d056f9c2db8538511211b79584b1
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-11-08 20:57:32 +0530

    feat: add controller to return specific order details

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -8,8 +8,15 @@ class AdminController extends Controller
 {
     public function index()
     {
-        $orders = Order::with(['products.product', 'user'])->get();
+        $orders = Order::with(['products.product', 'user', 'address'])->get();
 
         return view('adminorder', compact('orders'));
+    }
+
+    public function specific($id)
+    {
+        $order = Order::where('id', $id)->with(['products.product', 'user', 'address'])->get();
+
+        return view('adminorderspecific', compact('order'));
     }
 }
