64534031c9edc8e8a13e1f85ca92302fd9cc657a
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-11-08 16:27:15 +0530

    feat: add form to handle attribute value

--- resources/views/attribute.blade.php
+++ resources/views/attribute.blade.php
@@ -5,7 +5,7 @@
 <main class="space-y-6">
     <div>
         <div>
-            <h2 class="text-3xl font-bold mb-4">Add Category</h2>
+            <h2 class="text-3xl font-bold mb-4">Add Attribute</h2>
             <form class="bg-white p-6 rounded-lg shadow-lg" action="{{route('attribute.add')}}" method="post">
                 @csrf
 
@@ -14,6 +14,32 @@
                 </div>
                 <button type="submit" class="btn btn-primary ms-auto">
                     Add Attribute
+                </button>
+            </form>
+        </div>
+    </div>
+
+    <div>
+        <div>
+            <h2 class="text-3xl font-bold mb-4">Add Attribute Value</h2>
+            <form class="bg-white p-6 rounded-lg shadow-lg" action="{{route('attribute.value.add')}}" method="post">
+                @csrf
+
+                <div class="mb-3">
+                    <label class="form-label required">Attribute Value</label>
+                    <select class="form-select" id="attribute_id" name="attribute_id">
+                        <option value=""> Select Attribute </option>
+                        @foreach ($attributes as $attribute)
+                            <option value="{{ $attribute['id'] }}">{{ $attribute['name'] }}</option>
+                        @endforeach
+                    </select>
+                </div>
+
+                <div class="mb-3">
+                    <input type="text" class="form-control" id="value" name="value" placeholder="Enter Attribute Value">
+                </div>
+                <button type="submit" class="btn btn-primary ms-auto">
+                    Add Attribute Value
                 </button>
             </form>
         </div>
