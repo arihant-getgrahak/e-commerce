e260d6f9c7cfe59d41aeb31d8c5a9cb718d538e0
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-11-12 22:44:10 +0530

    feat: add enctype in form

--- resources/views/adminproductbulk.blade.php
+++ resources/views/adminproductbulk.blade.php
@@ -4,10 +4,14 @@
 
     <div>
         <h2 class="text-3xl font-bold mb-4">Upload file</h2>
-        <form class="bg-white p-6 rounded-lg shadow-lg" action="{{ route('admin.bulk.import') }}" method="POST">
+        <form class="bg-white p-6 rounded-lg shadow-lg" action="{{ route('admin.bulk.import') }}" method="POST"
+            enctype="multipart/form-data">
             @csrf
             <div class="mb-3">
                 <input type="file" class="form-control" id="file" name="file" placeholder="Upload file" accept=".csv">
+                @error("file")
+                    <p class="text-red-500">{{ $message }}</p>
+                @enderror
             </div>
             <button type="submit" class="btn btn-primary ms-auto">
                 Upload CSV
