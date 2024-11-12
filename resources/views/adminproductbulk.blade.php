137e399e71bd89c495d46c96ba6a87ef8e6b291f
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-11-12 22:33:14 +0530

    feat: make view to upload product

--- resources/views/adminproductbulk.blade.php
+++ resources/views/adminproductbulk.blade.php
@@ -1,3 +1,18 @@
-<div>
-    <h1>Admin Product Bulk</h1>
-</div>
+@extends('layout.index')
+@section('bulk')
+<main class="space-y-6">
+
+    <div>
+        <h2 class="text-3xl font-bold mb-4">Upload file</h2>
+        <form class="bg-white p-6 rounded-lg shadow-lg" action="{{ route('admin.bulk.import') }}" method="POST">
+            @csrf
+            <div class="mb-3">
+                <input type="file" class="form-control" id="file" name="file" placeholder="Upload file" accept=".csv">
+            </div>
+            <button type="submit" class="btn btn-primary ms-auto">
+                Upload CSV
+            </button>
+        </form>
+    </div>
+</main>
+@endsection
