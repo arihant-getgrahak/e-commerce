94dcb277617144c16fef23ca139249f21a7aaea5
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-11-13 19:09:03 +0530

    feat: add field to manage states

--- database/migrations/2024_11_13_190201_delivery_state.php
+++ database/migrations/2024_11_13_190201_delivery_state.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
@@ -9,7 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('delivery_states', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('country_id')->constrained('delivery_country')->onDelete('cascade');
+            $table->string('name');
+            $table->bollean('status')->default(true);
+            $table->timestamps();
+        });
     }
 
     /**
@@ -17,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('delivery_states');
     }
 };
