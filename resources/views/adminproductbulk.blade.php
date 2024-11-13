6175d587ae3c622dcd3e4c737e715ba4348aed14
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-11-13 19:00:24 +0530

    feat: add template in bulkproductupload view

--- resources/views/adminproductbulk.blade.php
+++ resources/views/adminproductbulk.blade.php
@@ -3,6 +3,12 @@
 <main class="space-y-6">
 
     <div>
+
+        <div class="d-flex align-items-center">
+            <h1 class="text-xl font-bold mb-4">Download Template before uploading file. <a class="btn btn-primary"
+                    href="{{asset("product_template.csv")}}">Download</a></h1>
+
+        </div>
         <h2 class="text-3xl font-bold mb-4">Upload file</h2>
         <form class="bg-white p-6 rounded-lg shadow-lg" action="{{ route('admin.bulk.import') }}" method="POST"
             enctype="multipart/form-data">
