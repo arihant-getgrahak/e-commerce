bd6534420efc2ee1fe1ae66652ddafeeafd7b625
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-11-13 14:12:33 +0530

    feat: add model to save search keyword in db

--- database/migrations/2024_11_13_125623_create_searches_table.php
+++ database/migrations/2024_11_13_125623_create_searches_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('search_keyword')->unique();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->integer('count');
             $table->timestamps();
         });
     }
