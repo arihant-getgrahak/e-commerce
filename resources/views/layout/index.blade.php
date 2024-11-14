e8e26f9d186de5922661baecfef6cf77e3248de0
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-11-14 13:18:18 +0530

    feat: update route name in layout/index

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -146,8 +146,8 @@
                             </li>
 
                             <!-- address -->
-                            <li @class(["nav-item", 'active' => Route::currentRouteName() === 'admin.address'])>
-                                <a class="nav-link" href="{{ route('admin.address') }}">
+                            <li @class(["nav-item", 'active' => Route::currentRouteName() === 'admin.country'])>
+                                <a class="nav-link" href="{{ route('admin.country') }}">
                                     <span class="nav-link-icon d-md-none d-lg-inline-block">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
