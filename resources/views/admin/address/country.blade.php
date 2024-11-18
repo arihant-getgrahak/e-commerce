ed6c7eab489268b0fe17b2a8f3d65896512a4deb
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-11-18 17:00:06 +0530

    feat: update state and country view

--- resources/views/admin/address/country.blade.php
+++ resources/views/admin/address/country.blade.php
@@ -37,42 +37,6 @@
 </main>
 
 
-
-
-
-{{--<div class="modal modal-blur fade" id="modal-team" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">Update City</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <form id="productForm" class="bg-white p-6 rounded-lg shadow-lg" method="post" action="">
-                    @csrf
-                    <!-- Name -->
-                    <div class="mb-4">
-                        <label for="name" class="col-form-label required">City Name</label>
-                        <input type="text" id="name" name="name"
-                            class="mt-1 block w-full p-2 border border-gray-300 rounded-md"
-                            placeholder="Enter product name">
-                    </div>
-                    <div class="mt-3">
-                        <label class="form-label required">IsActive</label>
-                        <input type="radio" name="status" id="status-city" value="1"> Yes
-                        <input type="radio" name="status" id="status-city" value="0"> No
-                    </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn me-auto" data-bs-dismiss="modal">Close</button>
-                <button type="submit" class="btn btn-primary ms-auto">Update</button>
-            </div>
-            </form>
-        </div>
-    </div>
-</div>--}}
-
-
 <script>
     if ("{{ session('success') }}") {
         alert("{{ session('success') }}");
