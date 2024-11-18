3d962cb9d0cfa27b368f9779f2afac6663e83534
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-11-18 17:14:45 +0530

    feat: add modal to update city name and status

--- resources/views/admin/address/city.blade.php
+++ resources/views/admin/address/city.blade.php
@@ -27,7 +27,9 @@
                                 <td class="sort-name">{{ $s->name }}</td>
                                 <td class="sort-name">{{ $s->state->name }}</td>
                                 <td>
-                                    <button class="btn btn-primary btn-sm btn-update">Edit</button>
+                                    <button class="btn btn-primary btn-sm btn-update" id="btn-update" data-bs-toggle="modal"
+                                        data-bs-target="#modal-city-update" data-id="{{ $s->id }}"
+                                        data-name="{{ $s->name }}" data-status="{{ $s->status }}">Edit</button>
                                     <button class="btn btn-danger btn-sm" data-bs-toggle="modal"
                                         data-bs-target="#modal-delete-{{$s->id}}">Delete</button>
                                 </td>
@@ -85,11 +87,7 @@
     </div>
 </main>
 
-
-
-
-
-
+<!-- Add City Modal -->
 <div class="modal modal-blur fade" id="cityModal" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
@@ -142,6 +140,39 @@
     </div>
 </div>
 
+<!-- Update City Modal -->
+<div class="modal modal-blur fade" id="modal-city-update" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Update City</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form id="cityUpdateForm" class="bg-white p-6 rounded-lg shadow-lg" method="post" action="">
+                    @csrf
+                    <!-- Name -->
+                    <div class="mb-4">
+                        <label for="name" class="col-form-label required">City Name</label>
+                        <input type="text" id="name" name="name"
+                            class="mt-1 block w-full p-2 border border-gray-300 rounded-md"
+                            placeholder="Enter product name">
+                    </div>
+                    <div class="mt-3">
+                        <label class="form-label required">IsActive</label>
+                        <input type="radio" name="status" id="status" value="1"> Yes
+                        <input type="radio" name="status" id="status" value="0"> No
+                    </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn me-auto" data-bs-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary ms-auto">Update</button>
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
+
 
 <script>
     if ("{{ session('success') }}") {
@@ -176,5 +207,32 @@
             state.innerHTML += HTML;
         });
     })
+</script>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+        const updateButtons = document.querySelectorAll('.btn-update');
+        const updateForm = document.getElementById("cityUpdateForm");
+
+        updateButtons.forEach(button => {
+            button.addEventListener('click', function () {
+                const id = this.getAttribute('data-id');
+                const name = this.getAttribute('data-name');
+                const status = this.getAttribute('data-status');
+                const statusBool = Boolean(status);
+
+
+                // updateForm.action = "{{ route('category.update', ':id') }}".replace(':id', productId);
+
+                updateForm.querySelector('#name').value = name;
+                if (statusBool) {
+                    document.querySelector('input[id="status"][value="1"]').checked = true;
+                } else {
+                    document.querySelector('input[id="status"][value="0"]').checked = true;
+                }
+            });
+        });
+    });
+
 </script>
 @endsection
