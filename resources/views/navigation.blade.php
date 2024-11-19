5e6b33b01cf8ad2c0e7f809a647b67d8949e4dce
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-11-19 21:56:15 +0530

    feat: update navigation view

--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -2,6 +2,9 @@
 @section('navigation')
 
 <div class="d-flex justify-content-between mb-4">
+    <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modal-menu">
+        Add Menu
+    </button>
     <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modal-link">
         Add Link
     </button>
@@ -9,49 +12,39 @@
 <div class="card">
     <div class="card-header">
         <ul class="nav nav-tabs card-header-tabs" data-bs-toggle="tabs">
-            <li class="nav-item">
-                <a href="#tabs-home" class="nav-link active" data-bs-toggle="tab">Header</a>
-            </li>
-            <li class="nav-item">
-                <a href="#tabs-footer" class="nav-link" data-bs-toggle="tab">Footer</a>
-            </li>
-
+            @foreach ($navigation as $nav)
+                <li class="nav-item">
+                    <a href="#tabs-{{$nav->id}}" class="nav-link" data-bs-toggle="tab">{{ $nav->name }}</a>
+                </li>
+            @endforeach
         </ul>
     </div>
     <div class="card-body">
         <div class="tab-content">
-            @foreach (['Header' => 'tabs-home', 'Footer' => 'tabs-footer'] as $type => $id)
-                        <div class="tab-pane {{ $type === 'Header' ? 'active show' : '' }}" id="{{ $id }}">
-                            @php
-                                $navigationItem = $navigation->firstWhere('name', $type);
-                            @endphp
-                            @if ($navigationItem && $navigationItem->menus->isNotEmpty())
-                                <ul class="nested-sortable">
-                                    @foreach ($navigationItem->menus as $menu)
+            @foreach ($navigation as $index => $nav)
+                <div class="tab-pane {{ $index === 0 ? 'active show' : '' }}" id="tabs-{{$nav->id}}">
+                    @foreach ($nav->menus as $menu)
+                        <ul>
+                            <li>{{ $menu->name }} (<a href="{{ $menu->link }}">{{ $menu->link }}</a>)</li>
+                            @if ($menu->children->isNotEmpty())
+                                <ul>
+                                    @foreach ($menu->children as $child)
                                         <li class="sort-name">
-                                            {{ $menu->name }} (<a href="{{ $menu->link }}">{{ $menu->link }}</a>)
-                                            @if ($menu->children->isNotEmpty())
-                                                <ul>
-                                                    @foreach ($menu->children as $child)
-                                                        <li class="sort-name">
-                                                            {{ $child->name }} (<a href="{{ $child->link }}">{{ $child->link }}</a>)
-                                                        </li>
-                                                    @endforeach
-                                                </ul>
-                                            @endif
+                                            {{ $child->name }} (<a href="{{ $child->link }}">{{ $child->link }}</a>)
                                         </li>
                                     @endforeach
                                 </ul>
-                            @else
-                                <p>No Children</p>
                             @endif
-                        </div>
+                        </ul>
+                    @endforeach
+                </div>
             @endforeach
         </div>
     </div>
 </div>
 
 
+<!-- add link modal -->
 <div class="modal modal-blur fade" id="modal-link" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
@@ -102,6 +95,40 @@
             <div class="modal-footer">
                 <button type="button" class="btn me-auto" data-bs-dismiss="modal">Close</button>
                 <button type="submit" class="btn btn-primary ms-auto">Add Link</button>
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<!-- add menu modal -->
+<div class="modal modal-blur fade" id="modal-menu" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Add Menu Form</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form id="productForm" class="bg-white p-6 rounded-lg shadow-lg" method="post"
+                    action="{{route("menu.add")}}" enctype="multipart/form-data">
+                    @csrf
+
+                    <!-- Name -->
+                    <div class="mb-4">
+                        <label for="name" class="col-form-label required">Name</label>
+                        <input type="text" id="name" name="name"
+                            class="mt-1 block w-full p-2 border border-gray-300 rounded-md" placeholder="Enter name"
+                            required>
+
+                        @error('name')
+                            <span class="text-danger">{{$message}}</span>
+                        @enderror
+                    </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn me-auto" data-bs-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary ms-auto">Add Menu</button>
             </div>
             </form>
         </div>
