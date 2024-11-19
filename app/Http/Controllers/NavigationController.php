6d1d6d25960be5f12e1fe526b835ecc0345df607
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-11-19 18:19:56 +0530

    feat: add function to add links in menu

--- app/Http/Controllers/NavigationController.php
+++ app/Http/Controllers/NavigationController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Navigation;
+use App\Models\NavigationMenu;
+use Illuminate\Http\Request;
+use Validator;
 
 class NavigationController extends Controller
 {
@@ -14,5 +17,40 @@ class NavigationController extends Controller
         }
 
         return view('navigation', compact('navigation'));
+    }
+
+    public function getLinks($id)
+    {
+        $links = NavigationMenu::where('navigation_id', $id)->whereNull('parent_id')->get();
+
+        if ($links) {
+            return response()->json(['links' => $links], 200);
+        }
+
+        return response()->json(['links' => []], 400);
+    }
+
+    public function store(Request $request)
+    {
+        $validate = Validator::make($request->all(), [
+            'name' => 'required',
+            'parent_id' => 'nullable|exists:navigation_menus,id',
+            'navigation_id' => 'required|exists:navigations,id',
+            'link' => 'required',
+        ]);
+
+        if ($validate->fails()) {
+            return back()->with('errors', $validate->errors());
+        }
+        $data = [
+            'user_id' => auth()->user()->id,
+            'navigation_id' => $request->navigation_id,
+            'parent_id' => $request->parent_id ?? null,
+            'name' => $request->name,
+            'link' => $request->link,
+        ];
+        NavigationMenu::create($data);
+
+        return back()->with('success', 'Navigation menu created successfully');
     }
 }
