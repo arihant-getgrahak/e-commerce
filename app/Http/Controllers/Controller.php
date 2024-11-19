6145fdd880ae90739bdaf0eeeaec99c41ccbd9a3
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-11-19 21:28:08 +0530

    feat: fix header and footer menu in frontend controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Navigation;
+use View;
+
 abstract class Controller
 {
-    //
+    public function __construct()
+    {
+        $navigation = Navigation::with('menus.children')->get();
+        foreach ($navigation as $n) {
+            $n->menus = $n->menus->whereNull('parent_id');
+        }
+
+        // view()->share('navigations', $navigation);
+        View::share('navigations', $navigation);
+    }
 }
