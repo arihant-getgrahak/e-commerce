03cbd8fafeacc617b3682c24c2001555a0f6b239
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-11-19 11:41:43 +0530

    feat: update invoice ui

--- resources/views/invoice.blade.php
+++ resources/views/invoice.blade.php
@@ -1,3 +1,4 @@
+{{--
 <!DOCTYPE html>
 <html lang="en">
 
@@ -87,26 +88,26 @@
                                             </thead>
                                             <tbody>
                                                 @foreach ($order->products as $product)
-                                                    <tr>
-                                                        <td style="padding-block: 12px;">
-                                                            <p style="font-weight: 700; color: #1A1C21;">
-                                                                {{Str::ucfirst($product->product->name)}}
-                                                            </p>
-                                                        </td>
-                                                        <td style="padding-block: 12px;">
-                                                            <p style="font-weight: 700; color: #1A1C21;">
-                                                                {{$product->quantity}}
-                                                            </p>
-                                                        </td>
-                                                        <td style="padding-block: 12px; text-align: end;">
-                                                            <p style="font-weight: 700; color: #1A1C21;">
-                                                                ₹{{$product->price / $product->quantity}}</p>
-                                                        </td>
-                                                        <td style="padding-block: 12px; text-align: end;">
-                                                            <p style="font-weight: 700; color: #1A1C21;">
-                                                                ₹{{$product->price}}</p>
-                                                        </td>
-                                                    </tr>
+                                                <tr>
+                                                    <td style="padding-block: 12px;">
+                                                        <p style="font-weight: 700; color: #1A1C21;">
+                                                            {{Str::ucfirst($product->product->name)}}
+                                                        </p>
+                                                    </td>
+                                                    <td style="padding-block: 12px;">
+                                                        <p style="font-weight: 700; color: #1A1C21;">
+                                                            {{$product->quantity}}
+                                                        </p>
+                                                    </td>
+                                                    <td style="padding-block: 12px; text-align: end;">
+                                                        <p style="font-weight: 700; color: #1A1C21;">
+                                                            ₹{{$product->price / $product->quantity}}</p>
+                                                    </td>
+                                                    <td style="padding-block: 12px; text-align: end;">
+                                                        <p style="font-weight: 700; color: #1A1C21;">
+                                                            ₹{{$product->price}}</p>
+                                                    </td>
+                                                </tr>
                                                 @endforeach
                                             </tbody>
                                             <tfoot>
@@ -139,7 +140,8 @@
                                                                         Total Price</th>
                                                                     <th
                                                                         style="padding: 12px 0 30px 0;text-align: end; color: #1A1C21;border-top:1px solid #D7DAE0;">
-                                                                        ₹{{$order->total_price + $order->total_price * 0.12}}
+                                                                        ₹{{$order->total_price + $order->total_price *
+                                                                        0.12}}
                                                                     </th>
                                                                 </tr>
                                                             </tfoot>
@@ -172,4 +174,110 @@
     </div>
 </body>
 
-</html>
+</html>--}}
+
+@extends('layout.index')
+
+
+@section("category")
+
+<div class="page-header d-print-none">
+    <div class="container-xl">
+        <div class="row g-2 align-items-center">
+            <div class="col">
+                <h2 class="page-title">
+                    Invoice
+                </h2>
+            </div>
+            <div class="col-auto ms-auto d-print-none">
+                <button type="button" class="btn btn-primary" onclick="javascript:window.print();">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
+                        stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+                        stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M17 17h2a2 2 0 0 0 2 -2v-4a2 2 0 0 0 -2 -2h-14a2 2 0 0 0 -2 2v4a2 2 0 0 0 2 2h2" />
+                        <path d="M17 9v-4a2 2 0 0 0 -2 -2h-6a2 2 0 0 0 -2 2v4" />
+                        <path d="M7 13m0 2a2 2 0 0 1 2 -2h6a2 2 0 0 1 2 2v4a2 2 0 0 1 -2 2h-6a2 2 0 0 1 -2 -2z" />
+                    </svg>
+                    Print Invoice
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Page body -->
+<div class="page-body">
+    <div class="container-xl">
+        <div class="card card-lg">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-6">
+                        <p class="h3">Arihant E-Commerce Services Pvt. Ltd.</p>
+                        <address>
+                            VidhyaDhar Nagar<br>
+                            Jaipur, Rajasthan<br>
+                            302039<br>
+                            arihantj916@gmail.com
+                        </address>
+                    </div>
+                    <div class="col-6 text-end">
+                        <p class="h3">{{Str::ucfirst($order->user->name)}}</p>
+                        <address>
+                            {{$order->address->address}}<br>
+                            {{$order->address->city}}, {{$order->address->state}}<br>
+                            {{$order->address->pincode}}<br>
+                            {{$order->user->email}}
+                        </address>
+                    </div>
+                    <div class="col-12 my-5">
+                        <h1>Invoice #INV/{{$order->id}}</h1>
+                    </div>
+                </div>
+                <table class="table table-transparent table-responsive">
+                    <thead>
+                        <tr>
+                            <th class="text-center" style="width: 1%"></th>
+                            <th>Product</th>
+                            <th class="text-center" style="width: 1%">Qnt</th>
+                            <th class="text-end" style="width: 1%">Unit</th>
+                            <th class="text-end" style="width: 1%">Amount</th>
+                        </tr>
+                    </thead>
+                    @foreach ($order->products as $product)
+                                    <tr>
+                                        <td class="text-center"></td>
+                                        <td>
+                                            <p class="strong mb-1">{{Str::ucfirst($product->product->name)}}</p>
+                                        </td>
+                                        <td class="text-center">
+                                            {{$product->quantity}}
+                                        </td>
+                                        <td class="text-end">₹{{$product->price / $product->quantity}}</td>
+                                        <td class="text-end">₹{{$product->price}}</td>
+                                    </tr>
+
+
+                                    <!-- Summary -->
+                                    <tr>
+                                        <td colspan="4" class="strong text-end">Subtotal</td>
+                                        <td class="text-end">₹{{$order->total_price}}</td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="4" class="strong text-end">GST</td>
+                                        <td class="text-end">12%</td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="4" class="font-weight-bold text-uppercase text-end">Total Due</td>
+                                        <td class="font-weight-bold text-end"> ₹{{$order->total_price + $order->total_price *
+                        0.12}}</td>
+                                    </tr>
+                    @endforeach
+                </table>
+                <p class="text-secondary text-center mt-5">Thank you for your order.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+@endsection
