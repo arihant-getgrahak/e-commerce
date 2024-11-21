c1359051c79f3b79156d2cd5fa985c21d1602c1d
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-11-21 19:20:27 +0530

    fix: update table name

--- database/migrations/2024_11_21_191718_add_col_to_order_address.php
+++ database/migrations/2024_11_21_191718_add_col_to_order_address.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('order_address', function (Blueprint $table) {
+        Schema::table('order_adresses', function (Blueprint $table) {
             $table->boolean('is_default')->default(false);
             $table->enum('type', ['billing', 'shipping'])->default('billing');
         });
