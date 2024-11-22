6ac215dbf0f47176f74f2154d74d71218d12f742
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-11-22 11:34:02 +0530

    feat: add fillable in pickupaddress model class

--- app/Models/PickupAddress.php
+++ app/Models/PickupAddress.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class PickupAddress extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'name',
+        'email',
+        'address',
+        'city',
+        'state',
+        'country',
+        'pincode',
+        'phone',
+        'is_default',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
 }
