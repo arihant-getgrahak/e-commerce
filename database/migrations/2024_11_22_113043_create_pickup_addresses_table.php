d441df37c6b840c053e376981f0ac978338bcb5d
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-11-22 11:32:34 +0530

    feat: add fields to pickup-address table

--- database/migrations/2024_11_22_113043_create_pickup_addresses_table.php
+++ database/migrations/2024_11_22_113043_create_pickup_addresses_table.php
@@ -13,6 +13,16 @@ return new class extends Migration
     {
         Schema::create('pickup_addresses', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->string('email');
+            $table->string('address');
+            $table->string('city');
+            $table->string('state');
+            $table->string('country');
+            $table->string('pincode');
+            $table->string('phone');
+            $table->boolean('is_default')->default(false);
             $table->timestamps();
         });
     }
