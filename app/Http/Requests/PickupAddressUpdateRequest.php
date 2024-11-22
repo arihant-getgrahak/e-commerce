caf832fa88a9948b9a96e69b5e28405ac0bcfc4b
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-11-22 11:50:31 +0530

    feat: add request for validate pickupaddressUpdateRequest

---
+++ app/Http/Requests/PickupAddressUpdateRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PickupAddressUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return auth()->check() && auth()->user()->role === 'admin';
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'nullable|string|unique:pickup_addresses,name',
+            'email' => 'nullable|email',
+            'address' => 'nullable|string',
+            'city' => 'nullable|string',
+            'state' => 'nullable|string',
+            'country' => 'nullable|string',
+            'pincode' => 'nullable|numeric|digits:6',
+            'phone' => 'nullable|numeric|digits:10',
+            'is_default' => 'nullable|boolean',
+        ];
+    }
+}
