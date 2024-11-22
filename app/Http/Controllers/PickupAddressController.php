fcd78c4cee58a9f60c8847def74214ebe5273f6a
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-11-22 11:50:51 +0530

    feat: add function to update pickup address in db

--- app/Http/Controllers/PickupAddressController.php
+++ app/Http/Controllers/PickupAddressController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\PickupAddressRequest;
+use App\Http\Requests\PickupAddressUpdateRequest;
 use App\Models\PickupAddress;
+use DB;
 
 class PickupAddressController extends Controller
 {
@@ -14,23 +16,48 @@ class PickupAddressController extends Controller
 
     public function store(PickupAddressRequest $request)
     {
-        $data = [
-            'user_id' => auth()->user()->id,
-            'name' => $request->name,
-            'phone' => $request->phone,
-            'address' => $request->address,
-            'city' => $request->city,
-            'state' => $request->state,
-            'pincode' => $request->pincode,
-            'country' => $request->country,
-        ];
+        try {
+            $data = [
+                'user_id' => auth()->user()->id,
+                'name' => $request->name,
+                'phone' => $request->phone,
+                'address' => $request->address,
+                'city' => $request->city,
+                'state' => $request->state,
+                'pincode' => $request->pincode,
+                'country' => $request->country,
+            ];
 
-        $create = PickupAddress::create($data);
+            DB::beginTransaction();
+            PickupAddress::create($data);
+            DB::commit();
 
-        if (! $create) {
-            return back()->with('error', 'Pickup address not created');
+            return back()->with('success', 'Pickup address created');
+        } catch (\Exception $e) {
+            DB::rollBack();
+
+            return back()->with('error', $e->getMessage());
         }
+    }
 
-        return back()->with('success', 'Pickup address created');
+    public function update($id, PickupAddressUpdateRequest $request)
+    {
+        try {
+            $update = PickupAddress::where('id', $id);
+            if (! $update) {
+                return back()->with('error', 'Pickup address not found');
+            }
+            $data = $request->only(['name', 'phone', 'address', 'city', 'state', 'pincode', 'country']);
+
+            DB::beginTransaction();
+            $update->update($data);
+            DB::commit();
+
+            return back()->with('success', 'Pickup address updated');
+        } catch (\Exception $e) {
+            DB::rollBack();
+
+            return back()->with('error', 'Pickup address not updated');
+        }
     }
 }
