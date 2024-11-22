befc3e4961876185a26c6b69a2ff804cd020f637
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-11-22 14:57:16 +0530

    feat: make pickup address view dynamic and add pagination

--- resources/views/admin/pickupaddress.blade.php
+++ resources/views/admin/pickupaddress.blade.php
@@ -1,7 +1,132 @@
 @extends('layout.index')
 
 @section('pickupaddress')
-@php
-    echo $addresses;
-@endphp
+<h1>Pickup Address</h1>
+<div class="col-12">
+    <div class="card">
+        <div class="table-responsive">
+            <table class="table table-vcenter card-table table-striped">
+                <thead>
+                    <tr>
+                        <th>Address NickName</th>
+                        <th>Address</th>
+                        <th>Email to be contacted</th>
+                        <th class="w-1"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($addresses as $address)
+                        <tr>
+                            <td>{{$address->name}}</td>
+                            <td class="text-secondary">
+                                {{$address->address}}, {{$address->city}}, {{$address->state}}, {{$address->pincode}}
+                            </td>
+                            <td class="text-secondary"><a href="#" class="text-reset">{{$address->email}}</a></td>
+                            <td>
+                                <a href="#">Edit</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+{{--<div class="mt-3">
+    {{$addresses->links()}}
+</div>
+
+<ul class="pagination ">
+    <li class="page-item disabled">
+        <a class="page-link" href="#" tabindex="-1" aria-disabled="true">
+            <!-- Download SVG icon from http://tabler-icons.io/i/chevron-left -->
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
+                stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M15 6l-6 6l6 6" />
+            </svg>
+            prev
+        </a>
+    </li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item active"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item"><a class="page-link" href="#">4</a></li>
+    <li class="page-item"><a class="page-link" href="#">5</a></li>
+    <li class="page-item">
+        <a class="page-link" href="#">
+            next <!-- Download SVG icon from http://tabler-icons.io/i/chevron-right -->
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
+                stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M9 6l6 6l-6 6" />
+            </svg>
+        </a>
+    </li>
+</ul>--}}
+
+<div class="mt-3">
+    <div class="mb-3 text-gray-600">
+        Showing {{ $addresses->firstItem() }} to {{ $addresses->lastItem() }} out of {{ $addresses->total() }} results
+    </div>
+    <ul class="pagination">
+        @if ($addresses->onFirstPage())
+            <li class="page-item disabled">
+                <span class="page-link" aria-disabled="true">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
+                        stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M15 6l-6 6l6 6" />
+                    </svg>
+                    Prev
+                </span>
+            </li>
+        @else
+            <li class="page-item">
+                <a class="page-link" href="{{ $addresses->previousPageUrl() }}" rel="prev">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
+                        stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M15 6l-6 6l6 6" />
+                    </svg>
+                    Prev
+                </a>
+            </li>
+        @endif
+
+        @foreach ($addresses->links()->elements[0] as $page => $url)
+            @if ($page == $addresses->currentPage())
+                <li class="page-item active"><span class="page-link">{{ $page }}</span></li>
+            @else
+                <li class="page-item"><a class="page-link" href="{{ $url }}">{{ $page }}</a></li>
+            @endif
+        @endforeach
+
+        @if ($addresses->hasMorePages())
+            <li class="page-item">
+                <a class="page-link" href="{{ $addresses->nextPageUrl() }}" rel="next">
+                    Next
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
+                        stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M9 6l6 6l-6 6" />
+                    </svg>
+                </a>
+            </li>
+        @else
+            <li class="page-item disabled">
+                <span class="page-link" aria-disabled="true">
+                    Next
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
+                        stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M9 6l6 6l-6 6" />
+                    </svg>
+                </span>
+            </li>
+        @endif
+    </ul>
+</div>
+
 @endsection
