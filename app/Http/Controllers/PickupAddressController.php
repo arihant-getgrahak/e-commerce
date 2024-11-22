b929e06af45389876dcffcc52ed8122bf4a351d3
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-11-22 11:43:53 +0530

    feat: add function to store  pickup address in db

--- app/Http/Controllers/PickupAddressController.php
+++ app/Http/Controllers/PickupAddressController.php
@@ -2,10 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\PickupAddressRequest;
+use App\Models\PickupAddress;
+
 class PickupAddressController extends Controller
 {
     public function index()
     {
         return view('admin.pickupaddress');
+    }
+
+    public function store(PickupAddressRequest $request)
+    {
+        $data = [
+            'user_id' => auth()->user()->id,
+            'name' => $request->name,
+            'phone' => $request->phone,
+            'address' => $request->address,
+            'city' => $request->city,
+            'state' => $request->state,
+            'pincode' => $request->pincode,
+            'country' => $request->country,
+        ];
+
+        $create = PickupAddress::create($data);
+
+        if (! $create) {
+            return back()->with('error', 'Pickup address not created');
+        }
+
+        return back()->with('success', 'Pickup address created');
     }
 }
