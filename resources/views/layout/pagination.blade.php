897ef34f2a78d0fd51a7b25b64916626be46d9cc
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-11-25 19:05:33 +0530

    fix: update paginatior view

--- resources/views/layout/pagination.blade.php
+++ resources/views/layout/pagination.blade.php
@@ -1,9 +1,9 @@
 <div class="mt-3 d-flex justify-content-between">
     <div class="mb-3 text-gray-600">
-        Showing {{ $addresses->firstItem() }} to {{ $addresses->lastItem() }} out of {{ $addresses->total() }} results
+        Showing {{ $paginator->firstItem() }} to {{ $paginator->lastItem() }} out of {{ $paginator->total() }} results
     </div>
     <ul class="pagination">
-        @if ($addresses->onFirstPage())
+        @if ($paginator->onFirstPage())
             <li class="page-item disabled">
                 <span class="page-link" aria-disabled="true">
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
@@ -16,7 +16,7 @@
             </li>
         @else
             <li class="page-item">
-                <a class="page-link" href="{{ $addresses->previousPageUrl() }}" rel="prev">
+                <a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev">
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
                         stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -27,17 +27,17 @@
             </li>
         @endif
 
-        @foreach ($addresses->links()->elements[0] as $page => $url)
-            @if ($page == $addresses->currentPage())
+        @foreach ($paginator->links()->elements[0] as $page => $url)
+            @if ($page == $paginator->currentPage())
                 <li class="page-item active"><span class="page-link">{{ $page }}</span></li>
             @else
                 <li class="page-item"><a class="page-link" href="{{ $url }}">{{ $page }}</a></li>
             @endif
         @endforeach
 
-        @if ($addresses->hasMorePages())
+        @if ($paginator->hasMorePages())
             <li class="page-item">
-                <a class="page-link" href="{{ $addresses->nextPageUrl() }}" rel="next">
+                <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next">
                     Next
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
                         stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
