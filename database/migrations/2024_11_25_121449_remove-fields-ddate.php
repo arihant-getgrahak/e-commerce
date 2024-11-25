98bb9925324dd719114acf268839029b7d54e710
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-11-25 12:16:00 +0530

    feat: remove delivery date in orders table

---
+++ database/migrations/2024_11_25_121449_remove-fields-ddate.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('orders', function ($table) {
+            $table->dropColumn('delivery_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('orders', function ($table) {
+            $table->datetime('delivery_date');
+        });
+    }
+};
