4beb1ba9f766d8525579044787cf09b5add8f6fb
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-11-26 15:56:21 +0530

    feat: add helper function to fetch country code

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -135,63 +135,11 @@ class AuthController extends Controller
     public function registerView()
     {
 
-        $ip = request()->ip();
-        // $ip = "146.70.244.118";
-        $data = $this->getLocationInfo($ip);
-        $telcode = $this->getTelCode($data['data']['country'] ?? 'IN');
+        // $ip = request()->ip();
+        $ip = '146.70.245.84';
+        $data = getLocationInfo($ip);
+        $telcode = getTelCode($data['data']['country'] ?? 'IN');
 
         return view('register', compact('telcode'));
-    }
-
-    protected function getTelCode($data)
-    {
-        $response = Http::get("https://restcountries.com/v3.1/alpha/{$data}");
-        if ($response->ok()) {
-            $data = $response->json();
-            $telephoneCode = $data[0]['idd']['root'].$data[0]['idd']['suffixes'][0];
-
-            return [
-                'code' => $telephoneCode,
-                'status' => true,
-            ];
-        } else {
-            return [
-                'code' => 'Country not found.',
-                'status' => false,
-            ];
-        }
-    }
-
-    protected function getLocationInfo(string $ip): array
-    {
-        try {
-            $response = Http::get("http://ipinfo.io/{$ip}/json");
-
-            if ($response->successful()) {
-                $data = $response->json();
-
-                if (isset($data['bogon']) && $data['bogon'] == 1) {
-                    return [
-                        'status' => false,
-                        'message' => 'Bogon IP address detected. Unable to determine location.',
-                    ];
-                }
-
-                return [
-                    'status' => true,
-                    'data' => $data,
-                ];
-            }
-
-            return [
-                'status' => false,
-                'message' => 'Unable to retrieve location data.',
-            ];
-        } catch (\Throwable $th) {
-            return [
-                'status' => false,
-                'message' => 'An error occurred while fetching location data.',
-            ];
-        }
     }
 }
