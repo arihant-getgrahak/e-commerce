4caf74d1991275f47efce5928f31a1bd31907e5b
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-11-27 16:16:06 +0530

    feat: add dimensions in products table

---
+++ database/migrations/2024_11_27_161214_add_fields_in_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->string('length')->default('10');
+            $table->string('breath')->default('5');
+            $table->string('height')->default('1');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('length');
+            $table->dropColumn('breath');
+            $table->dropColumn('height');
+        });
+    }
+};
