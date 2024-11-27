cef963776ca9a768fc10620d1bbd6403b47c9324
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-11-27 12:17:08 +0530

    feat: show country based on authenticated status

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,14 +9,20 @@ abstract class Controller
 {
     public function __construct()
     {
-        $navigation = Navigation::with('menus.children')->get();
-        foreach ($navigation as $n) {
-            $n->menus = $n->menus->whereNull('parent_id');
+        $navigation = Navigation::with([
+            'menus' => function ($query) {
+                $query->whereNull('parent_id')->with('children');
+            },
+        ])->get();
+
+        $telcode = session('country', 'IN');
+        if (auth()->check()) {
+            $telcode = auth()->user()->country ?? $telcode;
+        } elseif (! session('country')) {
+            $ip = request()->ip() ?? '146.70.245.84';
+            $data = getLocationInfo($ip);
+            $telcode = $data['data']['country'] ?? $telcode;
         }
-        // $ip = request()->ip();
-        $ip = '146.70.245.84';
-        $data = getLocationInfo($ip);
-        $telcode = $data['data']['country'] ?? 'IN';
 
         View::share('navigations', compact('navigation', 'telcode'));
     }
