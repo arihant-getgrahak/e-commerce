0612bda7b398317c884e14feb118786533039cf9
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-11-28 12:04:22 +0530

    feat: add func to update shiprocket credentials

--- app/Http/Controllers/ShipRocketController.php
+++ app/Http/Controllers/ShipRocketController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\PickupAddress;
 use App\Models\Shipments;
 use Http;
+use Request;
 
 class ShipRocketController extends Controller
 {
@@ -125,5 +126,22 @@ class ShipRocketController extends Controller
                 'message' => 'Shipment not created',
             ];
         }
+    }
+
+    public function changeCredentials(Request $request)
+    {
+        $data = $request->only(['username', 'password', 'channelId']);
+
+        config([
+            'shiprocket.email' => $data['email'],
+            'shiprocket.password' => $data['password'],
+            'shiprocket.channelId' => $data['channelId'],
+        ]);
+
+        if (env('shiprocket.email') === $data['email'] && env('shiprocket.password') === $data['password'] && env('shiprocket.channelId') === $data['channelId']) {
+            return back()->with('success', 'Credentials updated');
+        }
+
+        return back()->with('error', 'Credentials not updated');
     }
 }
