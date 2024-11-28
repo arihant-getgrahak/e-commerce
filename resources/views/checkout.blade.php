b0dfffd07eb80c80692c3b2ff73368c428413d5c
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-11-28 22:23:17 +0530

    feat: optimize checkout blade view

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -244,7 +244,7 @@
     if ("{{session('success')}}") {
         arihant.innerHTML = `
             <div class="alert alert-success alert-dismissible fade show" role="alert">
-        ${{{ session('success') }}}
+        {{{ session('success') }}}
                                               <button type="button" class="close" data-dismiss="alert" aria-label="Close">
 												<span aria-hidden="true">&times;</span>
 											  </button>
@@ -255,7 +255,7 @@
     if ("{{session('error')}}") {
         arihant.innerHTML = `
             <div class="alert alert-danger alert-dismissible fade show" role="alert">
-        ${{{ session('error') }}}
+        {{{ session('error') }}}
                                               <button type="button" class="close" data-dismiss="alert" aria-label="Close">
 												<span aria-hidden="true">&times;</span>
 											  </button>
