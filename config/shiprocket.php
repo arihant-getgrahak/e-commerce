39fa7109fd99f25c883161022798ffb8017f5e83
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-11-28 12:00:37 +0530

    feat: add env var in config

---
+++ config/shiprocket.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'email' => env('SHIPROCKET_USERNAME', 'Laravel'),
+    'password' => env('SHIPROCKET_PASSWORD', 'Laravel'),
+    'token' => env('SHIPROCKET_TOKEN', 'Laravel'),
+    'channelId' => env('SHIPROCKET_CHANNEL_ID', 'Laravel'),
+];
