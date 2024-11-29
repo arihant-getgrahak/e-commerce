ff911c68cea68d8197a68e28b732d37a2ee5d144
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-11-29 15:23:16 +0530

    fix: add shipping address form

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -133,11 +133,10 @@
                                 <textarea class="form-control ht-50"></textarea>
                             </div>
                         </div>
-
                     </div>
 
                     @if(!auth()->check())
-                        <div class="row mb-4">
+                        <div class="row">
                             <div class="col-12 d-block">
                                 <input id="create_account_checkbox" class="checkbox-custom collapsed" name="createaccount"
                                     type="checkbox" data-toggle="collapse" data-target="#create_account_collapse"
@@ -155,6 +154,121 @@
                             </div>
                         </div>
                     @endif
+
+                    <div class="row mb-4">
+                        <div class="col-12 d-block">
+                            <input id="shipping_address" class="checkbox-custom collapsed" name="shipping_address"
+                                type="checkbox" data-toggle="collapse" data-target="#shippingAddress"
+                                aria-expanded="false" role="button">
+                            <label for="shipping_address" class="checkbox-custom-label">Add Shipping
+                                Address</label>
+
+                            <div id="shippingAddress" class="collapse">
+                                <h5 class="mb-4 ft-medium mt-4">Shipping Details</h5>
+                                <div class="row mb-2">
+                                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                                        <div class="form-group">
+                                            <label id="fname" class="text-dark">First Name *</label>
+                                            <input type="text" class="form-control" placeholder="First Name" id="sfname"
+                                                name="fname" required />
+                                        </div>
+                                    </div>
+
+                                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                                        <div class="form-group">
+                                            <label id="lname" class="text-dark">Last Name *</label>
+                                            <input type="text" class="form-control" placeholder="Last Name" id="slname"
+                                                name="lname" required />
+                                        </div>
+                                    </div>
+
+                                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                        <div class="form-group">
+                                            <label id="email" class="text-dark">Email *</label>
+                                            <input type="email" class="form-control" placeholder="Email" id="semail"
+                                                name="email" required />
+                                        </div>
+                                    </div>
+
+                                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                        <div class="form-group">
+                                            <label class="text-dark" id="address1">Address 1 *</label>
+                                            <input type="text" class="form-control" placeholder="Address 1"
+                                                id="address1" name="saddress1" required />
+                                        </div>
+                                    </div>
+
+                                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                        <div class="form-group">
+                                            <label class="text-dark" id="address2">Address 2</label>
+                                            <input type="text" class="form-control" placeholder="Address 2"
+                                                id="address2" name="saddress2" />
+                                        </div>
+                                    </div>
+
+                                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                        <div class="form-group">
+                                            <label id="country" class="text-dark">Country *</label>
+                                            <select class="custom-select" id="country" name="scountry" required>
+                                                <option value="India" selected="">India</option>
+                                                <option value="United State">United State</option>
+                                                <option value="United Kingdom">United Kingdom</option>
+                                                <option value="China">China</option>
+                                                <option value="Germany">Germany</option>
+                                            </select>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                        <div class="form-group">
+                                            <label class="text-dark" id="state">State *</label>
+                                            <input type="text" class="form-control" placeholder="State" name="sstate"
+                                                id="state" required />
+                                        </div>
+                                    </div>
+
+                                    <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
+                                        <div class="form-group">
+                                            <label class="text-dark" id="city">City / Town *</label>
+                                            <input type="text" class="form-control" placeholder="City / Town"
+                                                name="scity" id="city" required />
+                                        </div>
+                                    </div>
+
+                                    <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
+                                        <div class="form-group">
+                                            <label class="text-dark" id="pincode">ZIP / Postcode *</label>
+                                            <input type="text" class="form-control" id="pincode" name="spincode"
+                                                placeholder="Zip / Postcode" required />
+                                        </div>
+                                    </div>
+
+                                    <div class="col-xl-2 col-lg-6 col-md-12 col-sm-12 col-12">
+                                        <div class="form-group">
+                                            <label class="text-dark" id="phone">Tel Code *</label>
+                                            <input type="text" class="form-control" placeholder="+91" name="sccode"
+                                                id="ccode" value="{{$telcode}}" required />
+                                        </div>
+                                    </div>
+
+                                    <div class="col-xl-10 col-lg-6 col-md-12 col-sm-12 col-12">
+                                        <div class="form-group">
+                                            <label class="text-dark" id="phone">Mobile Number *</label>
+                                            <input type="text" class="form-control" placeholder="Mobile Number"
+                                                name="sphone" id="phone" required />
+                                        </div>
+                                    </div>
+
+                                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                        <div class="form-group">
+                                            <label class="text-dark">Additional Information</label>
+                                            <textarea class="form-control ht-50"></textarea>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
                     <h5 class="mb-4 ft-medium">Payments</h5>
                     <div class="row mb-4">
@@ -178,9 +292,7 @@
                             </div>
                         </div>
                     </div>
-
                     <button type="submit" class="btn btn-block btn-dark">Checkout</button>
-
                 </form>
             </div>
 
@@ -206,8 +318,7 @@
                                             <div class="cart_single_caption pl-2">
                                                 <h4 class="product_title fs-md ft-medium mb-1 lh-1">{{$c->products[0]->name}}
                                                 </h4>
-                                                <!-- <p class="mb-1 lh-1"><span class="text-dark">Size: 40</span></p>
-                                                                                                                                                            <p class="mb-3 lh-1"><span class="text-dark">Color: Blue</span></p> -->
+                                                <p class="mb-3 lh-1"><span class="text-dark">Color: Blue</span></p> -->
                                                 <h4 class="fs-md ft-medium mb-3 lh-1">₹{{$c->price}}</h4>
                                             </div>
                                         </div>
