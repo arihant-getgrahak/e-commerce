9b2551b07b1bba03cf722530517208775a40035c
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-12-02 21:26:21 +0530

    feat: add tax_values and tax_type col to products table

---
+++ database/migrations/2024_12_02_212348_add_cols_tax_values_in_products_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->float('tax_value')->nullable();
+            $table->enum('tax_type', ['inclusive', 'exclusive'])->default('inclusive')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('tax_value');
+            $table->dropColumn('tax_type');
+        });
+    }
+};
