fc9a5581506825568c4ccbd33e28ff69457def8a
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-12-02 15:50:35 +0530

    feat: return tax data to view

--- app/Http/Controllers/AdminSettingController.php
+++ app/Http/Controllers/AdminSettingController.php
@@ -29,7 +29,7 @@ class AdminSettingController extends Controller
 
     public function taxView()
     {
-        $tax = Tax::where('user_id', auth()->user()->id)->get();
+        $tax = Tax::where('user_id', auth()->user()->id)->orderBy('created_at', 'desc')->paginate(10);
 
         return view('admin.setting.tax', compact('tax'));
     }
