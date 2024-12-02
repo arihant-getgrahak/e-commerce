4ca0c6962652582d9cddd4f4169eb0f965155e78
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-12-02 20:48:50 +0530

    feat: add table to view all stores

--- resources/views/admin/setting/store.blade.php
+++ resources/views/admin/setting/store.blade.php
@@ -7,78 +7,86 @@
         Add Store
     </button>
 
-    {{--<div class="card">
+    <div class="card mt-3">
         <div class="card-body">
-            <h1>All Category</h1>
+            <h1>All Stores</h1>
             <div id="table-default" class="table-responsive">
                 <table class="table">
                     <thead>
                         <tr>
-                            <th><button class="table-sort" data-sort="sort-name">Id</button></th>
-                            <th><button class="table-sort" data-sort="sort-city">Tax Value</button></th>
-                            <th><button class="table-sort" data-sort="sort-city">Tax Type</button></th>
-                            <th><button class="table-sort" data-sort="sort-type">Action</button></th>
+                            <th><button class="table-sort">Id</button></th>
+                            <th><button class="table-sort">Store Name</button></th>
+                            <th><button class="table-sort">Store Address</button></th>
+                            <th><button class="table-sort">Store Phone</button></th>
+                            <th><button class="table-sort">Store GST</button></th>
+                            <th><button class="table-sort">Store Tax Value</button></th>
+                            <th><button class="table-sort">Store Tax Type</button></th>
+                            <th><button class="table-sort">Action</button></th>
                         </tr>
                     </thead>
                     <tbody class="table-tbody">
-                        @foreach ($tax as $t)
-                        <tr>
-                            <td class="sort-name">{{$t->id}}</td>
-                            <td class="sort-city">{{$t->value}}%</td>
-                            <td class="sort-city">{{Str::ucfirst($t->type)}}</td>
-                            <td class="sort-type">
-                                <button class="btn btn-primary btn-sm btn-update" data-bs-toggle="modal"
-                                    data-bs-target="#modal-tax-update" data-id="{{ $t->id }}"
-                                    data-value="{{ $t->value }}" data-type="{{ $t->type }}">Edit</button>
-                                <button class="btn btn-danger btn-sm" data-bs-toggle="modal"
-                                    data-bs-target="#modal-delete-{{$t->id}}">Delete</button>
-                            </td>
-                        </tr>
+                        @foreach ($store as $s)
+                            <tr>
+                                <td>{{$s->id}}</td>
+                                <td>{{$s->name}}</td>
+                                <td>{{$s->address}}, {{$s->city}}, {{$s->state}}, {{$s->pincode}}</td>
+                                <td>{{$s->phone}}</td>
+                                <td>{{$s->gst}}</td>
+                                <td>{{$s->tax_value}}%</td>
+                                <td>{{Str::ucfirst($s->tax_type)}}</td>
+                                <td>
+                                    <button class="btn btn-primary btn-sm btn-update" data-bs-toggle="modal"
+                                        data-bs-target="#modal-tax-update" data-id="{{ $s->id }}"
+                                        data-value="{{ $s->value }}" data-type="{{ $s->type }}">Edit</button>
+                                    <button class="btn btn-danger btn-sm" data-bs-toggle="modal"
+                                        data-bs-target="#modal-delete-{{$s->id}}">Delete</button>
+                                </td>
+                            </tr>
 
-                        <!-- Delete Modal -->
-                        <div class="modal modal-blur fade" id="modal-delete-{{$t->id}}" tabindex="-1" role="dialog"
-                            aria-hidden="true">
-                            <div class="modal-dialog modal-sm modal-dialog-centered" role="document">
-                                <div class="modal-content">
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                        aria-label="Close"></button>
-                                    <div class="modal-status bg-danger"></div>
-                                    <div class="modal-body text-center py-4">
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon mb-2 text-danger icon-lg"
-                                            width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
-                                            stroke="currentColor" fill="none" stroke-linecap="round"
-                                            stroke-linejoin="round">
-                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                            <path
-                                                d="M10.24 3.957l-8.422 14.06a1.989 1.989 0 0 0 1.7 2.983h16.845a1.989 1.989 0 0 0 1.7 -2.983l-8.423 -14.06a1.989 1.989 0 0 0 -3.4 0z" />
-                                            <path d="M12 9v4" />
-                                            <path d="M12 17h.01" />
-                                        </svg>
-                                        <h3>Are you sure?</h3>
-                                        <div class="text-secondary">Do you really want to delete this Tax? This
-                                            action cannot be undone.</div>
-                                    </div>
-                                    <div class="modal-footer">
-                                        <div class="w-100">
-                                            <div class="row">
-                                                <div class="col"><a href="#" class="btn w-100"
-                                                        data-bs-dismiss="modal">Cancel</a></div>
-                                                <div class="col">
-                                                    <form id="delete-form-{{$t->id}}"
-                                                        action="{{ route('admin.setting.tax.delete', $t->id) }}"
-                                                        method="POST" class="w-100">
-                                                        @csrf
-                                                        @method('DELETE')
-                                                        <button type="submit" class="btn btn-danger w-100">Delete
-                                                            Tax</button>
-                                                    </form>
+                            <!-- Delete Modal -->
+                            <div class="modal modal-blur fade" id="modal-delete-{{$s->id}}" tabindex="-1" role="dialog"
+                                aria-hidden="true">
+                                <div class="modal-dialog modal-sm modal-dialog-centered" role="document">
+                                    <div class="modal-content">
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                            aria-label="Close"></button>
+                                        <div class="modal-status bg-danger"></div>
+                                        <div class="modal-body text-center py-4">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="icon mb-2 text-danger icon-lg"
+                                                width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
+                                                stroke="currentColor" fill="none" stroke-linecap="round"
+                                                stroke-linejoin="round">
+                                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                <path
+                                                    d="M10.24 3.957l-8.422 14.06a1.989 1.989 0 0 0 1.7 2.983h16.845a1.989 1.989 0 0 0 1.7 -2.983l-8.423 -14.06a1.989 1.989 0 0 0 -3.4 0z" />
+                                                <path d="M12 9v4" />
+                                                <path d="M12 17h.01" />
+                                            </svg>
+                                            <h3>Are you sure?</h3>
+                                            <div class="text-secondary">Do you really want to delete this Store? This
+                                                action cannot be undone.</div>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <div class="w-100">
+                                                <div class="row">
+                                                    <div class="col"><a href="#" class="btn w-100"
+                                                            data-bs-dismiss="modal">Cancel</a></div>
+                                                    <div class="col">
+                                                        <form id="delete-form-{{$s->id}}"
+                                                            action="{{ route('admin.setting.store.delete', $s->id) }}"
+                                                            method="POST" class="w-100">
+                                                            @csrf
+                                                            @method('DELETE')
+                                                            <button type="submit" class="btn btn-danger w-100">Delete
+                                                                Store</button>
+                                                        </form>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
                         @endforeach
                     </tbody>
                 </table>
@@ -86,7 +94,7 @@
         </div>
     </div>
 
-    <div class="modal modal-blur fade" id="modal-tax-update" tabindex="-1" role="dialog" aria-hidden="true">
+    {{--<div class="modal modal-blur fade" id="modal-tax-update" tabindex="-1" role="dialog" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header">
