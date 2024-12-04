2c4f30a8c120198d5daff9235a4783a6adc4fc85
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-12-04 10:42:07 +0530

    feat: update route delete forex in forex blade view

--- resources/views/admin/setting/forex.blade.php
+++ resources/views/admin/setting/forex.blade.php
@@ -25,71 +25,70 @@
                         </tr>
                     </thead>
                     <tbody class="table-tbody">
-                        {{--@foreach ($store as $s)
-                        <tr>
-                            <td>{{$s->id}}</td>
-                            <td>{{$s->name}}</td>
-                            <td>{{$s->address}}, {{$s->city}}, {{$s->state}}, {{$s->pincode}}</td>
-                            <td>{{$s->phone}}</td>
-                            <td>{{$s->gst}}</td>
-                            <td>{{Str::ucfirst($s->tax_type)}}</td>
-                            <td>
-                                <button class="btn btn-primary btn-sm btn-update" data-bs-toggle="modal"
-                                    data-bs-target="#modal-tax-update" data-id="{{ $s->id }}" data-name="{{ $s->name }}"
-                                    data-address="{{ $s->address }}" data-phone="{{ $s->phone }}"
-                                    data-country="{{ $s->country }}" data-gst="{{ $s->gst }}" data-city="{{ $s->city }}"
-                                    data-state="{{ $s->state }}" data-pincode="{{ $s->pincode }}"
-                                    data-type="{{ $s->tax_type }}">Edit</button>
-                                <button class="btn btn-danger btn-sm" data-bs-toggle="modal"
-                                    data-bs-target="#modal-delete-{{$s->id}}">Delete</button>
-                            </td>
-                        </tr>
+                        @foreach ($forex as $f)
+                            <tr>
+                                <td>{{$f->id}}</td>
+                                <td>{{$f->name}}</td>
+                                <td>{{$f->symbol}}</td>
+                                <td>{{$f->code}}</td>
+                                <td>{{$f->exchange}}</td>
+                                <td>{{$f->status}}</td>
+                                <td>
+                                    <button class="btn btn-primary btn-sm btn-update" data-bs-toggle="modal"
+                                        data-bs-target="#modal-tax-update" data-id="{{ $f->id }}" data-name="{{ $f->name }}"
+                                        data-symbol="{{ $f->symbol }}" data-code="{{ $f->code }}"
+                                        data-exchange="{{ $f->exchange }}" data-status="{{ $f->status }}">Edit</button>
+                                    <button class="btn btn-danger btn-sm" data-bs-toggle="modal"
+                                        data-bs-target="#modal-delete-{{$f->id}}">Delete</button>
+                                </td>
+                            </tr>
 
-                        <!-- Delete Modal -->
-                        <div class="modal modal-blur fade" id="modal-delete-{{$s->id}}" tabindex="-1" role="dialog"
-                            aria-hidden="true">
-                            <div class="modal-dialog modal-sm modal-dialog-centered" role="document">
-                                <div class="modal-content">
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                        aria-label="Close"></button>
-                                    <div class="modal-status bg-danger"></div>
-                                    <div class="modal-body text-center py-4">
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon mb-2 text-danger icon-lg"
-                                            width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
-                                            stroke="currentColor" fill="none" stroke-linecap="round"
-                                            stroke-linejoin="round">
-                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                            <path
-                                                d="M10.24 3.957l-8.422 14.06a1.989 1.989 0 0 0 1.7 2.983h16.845a1.989 1.989 0 0 0 1.7 -2.983l-8.423 -14.06a1.989 1.989 0 0 0 -3.4 0z" />
-                                            <path d="M12 9v4" />
-                                            <path d="M12 17h.01" />
-                                        </svg>
-                                        <h3>Are you sure?</h3>
-                                        <div class="text-secondary">Do you really want to delete this Store? This
-                                            action cannot be undone.</div>
-                                    </div>
-                                    <div class="modal-footer">
-                                        <div class="w-100">
-                                            <div class="row">
-                                                <div class="col"><a href="#" class="btn w-100"
-                                                        data-bs-dismiss="modal">Cancel</a></div>
-                                                <div class="col">
-                                                    <form id="delete-form-{{$s->id}}"
-                                                        action="{{ route('admin.setting.store.delete', $s->id) }}"
-                                                        method="POST" class="w-100">
-                                                        @csrf
-                                                        @method('DELETE')
-                                                        <button type="submit" class="btn btn-danger w-100">Delete
-                                                            Store</button>
-                                                    </form>
+                            <!-- Delete Modal -->
+                            <div class="modal modal-blur fade" id="modal-delete-{{$f->id}}" tabindex="-1" role="dialog"
+                                aria-hidden="true">
+                                <div class="modal-dialog modal-sm modal-dialog-centered" role="document">
+                                    <div class="modal-content">
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                            aria-label="Close"></button>
+                                        <div class="modal-status bg-danger"></div>
+                                        <div class="modal-body text-center py-4">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="icon mb-2 text-danger icon-lg"
+                                                width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
+                                                stroke="currentColor" fill="none" stroke-linecap="round"
+                                                stroke-linejoin="round">
+                                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                <path
+                                                    d="M10.24 3.957l-8.422 14.06a1.989 1.989 0 0 0 1.7 2.983h16.845a1.989 1.989 0 0 0 1.7 -2.983l-8.423 -14.06a1.989 1.989 0 0 0 -3.4 0z" />
+                                                <path d="M12 9v4" />
+                                                <path d="M12 17h.01" />
+                                            </svg>
+                                            <h3>Are you sure?</h3>
+                                            <div class="text-secondary">Do you really want to delete this Forex Currency?
+                                                This
+                                                action cannot be undone.</div>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <div class="w-100">
+                                                <div class="row">
+                                                    <div class="col"><a href="#" class="btn w-100"
+                                                            data-bs-dismiss="modal">Cancel</a></div>
+                                                    <div class="col">
+                                                        <form id="delete-form-{{$f->id}}"
+                                                            action="{{ route('admin.setting.forex.delete', $f->id) }}"
+                                                            method="POST" class="w-100">
+                                                            @csrf
+                                                            @method('DELETE')
+                                                            <button type="submit"
+                                                                class="btn btn-danger w-100">Delete</button>
+                                                        </form>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        @endforeach--}}
+                        @endforeach
                     </tbody>
                 </table>
             </div>
