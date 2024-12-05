918e954d524e86d9876dfd5b38c44bed0fcce47c
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-12-05 12:32:06 +0530

    feat: make user dashboard language dynamic

--- resources/views/layout/myaccount.blade.php
+++ resources/views/layout/myaccount.blade.php
@@ -19,20 +19,20 @@
 
                     <div class="dashboard_author">
                         <h4 class="px-3 py-2 mb-0 lh-2 gray fs-sm ft-medium text-muted text-uppercase text-left">
-                            Dashboard Navigation</h4>
+                            {{__("Dashboard Navigation")}}
+                        </h4>
                         <ul class="dahs_navbar">
                             <li><a href="{{route('my-orders')}}" @class(["", "active" => Route::currentRouteName() === 'my-orders'])><i
-                                        class="lni lni-shopping-basket mr-2"></i>My
-                                    Order</a></li>
+                                        class="lni lni-shopping-basket mr-2"></i>{{__("My Orders")}}</a></li>
                             <!-- @class(["nav-item", 'active' => Route::currentRouteName() === 'admin']) -->
                             <li><a href="{{route('wishlist')}}" @class(["", "active" => Route::currentRouteName() === "wishlist"])><i
-                                        class="lni lni-heart mr-2"></i>Wishlist</a></li>
-                            <li><a href="profile-info.html" @class(["", "active" => Route::currentRouteName() === "profile"])><i class="lni lni-user mr-2"></i>Profile
-                                    Info</a></li>
+                                        class="lni lni-heart mr-2"></i>{{__("Wishlist")}}</a></li>
+                            <li><a href="profile-info.html" @class(["", "active" => Route::currentRouteName() === "profile"])><i
+                                        class="lni lni-user mr-2"></i>{{__("Profile Info")}}</a></li>
                             <li><a href="{{route('address')}}" @class(["", "active" => Route::currentRouteName() === "address"])><i
-                                        class="lni lni-map-marker mr-2"></i>Addresses</a></li>
-                            <li><a href="{{ route('logout') }}" @class(["", "active" => Route::currentRouteName() === "logout"])><i class="lni lni-power-switch mr-2"></i>Log
-                                    Out</a></li>
+                                        class="lni lni-map-marker mr-2"></i>{{__("Addresses")}}</a></li>
+                            <li><a href="{{ route('logout') }}" @class(["", "active" => Route::currentRouteName() === "logout"])><i
+                                        class="lni lni-power-switch mr-2"></i>{{__("Log Out")}}</a></li>
 
                         </ul>
                     </div>
