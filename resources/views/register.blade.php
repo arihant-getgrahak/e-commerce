9eaf665591e78a06fd2d2d470046026231811a0f
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-12-05 09:52:41 +0530

    feat: display register data according to selected language

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -2,36 +2,36 @@
 
 @section("section")
 
-<section class="middle">
-    <div class="gray py-3">
-        <div class="container">
-            <div class="row">
-                <div class="colxl-12 col-lg-12 col-md-12">
-                    <nav aria-label="breadcrumb">
-                        <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="/">Home</a></li>
-                            <li class="breadcrumb-item"><a href="#">Register</a>
-                            </li>
-                        </ol>
-                    </nav>
-                </div>
+<div class="gray py-3">
+    <div class="container">
+        <div class="row">
+            <div class="colxl-12 col-lg-12 col-md-12">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="/">Home</a></li>
+                        <li class="breadcrumb-item"><a href="#">Register</a>
+                        </li>
+                    </ol>
+                </nav>
             </div>
         </div>
     </div>
+</div>
+<section class="middle">
     <div class="container">
         <form class="border p-3 rounded" action="{{ route('register') }}" method="post">
             @csrf
             <div class="row">
                 <div class="form-group col-md-6">
-                    <label for="name">Name *</label>
-                    <input type="text" class="form-control" placeholder="Name" required id="name" name="name"
+                    <label for="name">{{__("Name")}} *</label>
+                    <input type="text" class="form-control" placeholder="{{__("Name")}}" required id="name" name="name"
                         value="{{ old('name') }}">
                     @error('name')
                         <p class="text-red-500">{{ $message }}</p>
                     @enderror
                 </div>
                 <div class="form-group col-md-6">
-                    <label for="email">Email*</label>
+                    <label for="email">{{__("Email")}}*</label>
                     <input type="email" class="form-control" placeholder="joe@joe.com" required name="email" id="email"
                         value="{{ old('email') }}">
                     @error('email')
@@ -42,7 +42,7 @@
 
             <div class="row">
                 <div class="form-group col-md-6">
-                    <label for="country_code">Country Code *</label>
+                    <label for="country_code">{{__("Country Code")}} *</label>
                     <input type="text" class="form-control" placeholder="+91" required name="country_code"
                         id="country_code" value="{{ $telcode }}">
                     @error('country_code')
@@ -51,7 +51,7 @@
                 </div>
 
                 <div class="form-group col-md-6">
-                    <label for="phone_number">Phone Number *</label>
+                    <label for="phone_number">{{__("Phone Number")}} *</label>
                     <input type="tel" class="form-control" placeholder="9672670732" required name="phone_number"
                         id="phone_number" value="{{ old('phone_number') }}">
                     @error('phone_number')
@@ -62,8 +62,8 @@
 
             <div class="row">
                 <div class="form-group col-md-6">
-                    <label for="password">Password *</label>
-                    <input type="password" class="form-control" placeholder="Password*" required id="password"
+                    <label for="password">{{__("Password")}} *</label>
+                    <input type="password" class="form-control" placeholder="{{__("Password")}}*" required id="password"
                         name="password">
                     @error('password')
                         <p class="text-red-500">{{ $message }}</p>
@@ -71,8 +71,8 @@
                 </div>
 
                 <div class="form-group col-md-6">
-                    <label for="confirm_password">Confirm Password *</label>
-                    <input type="password" class="form-control" placeholder="Confirm Password*" required
+                    <label for="confirm_password">{{__("Confirm Password")}} *</label>
+                    <input type="password" class="form-control" placeholder="{{__("Confirm Password")}}*" required
                         id="confirm_password" name="confirm_password">
                     @error('confirm_password')
                         <p class="text-red-500">{{ $message }}</p>
@@ -81,21 +81,20 @@
             </div>
 
             <div class="form-group">
-                <p>By registering your details, you agree with our Terms & Conditions, and Privacy and Cookie
-                    Policy.</p>
+                <p>{{__("Register Agreement")}}</p>
             </div>
 
             <div class="form-group">
-                <button type="submit" class="btn btn-md full-width bg-dark text-light fs-md ft-medium">Create An
-                    Account</button>
+                <button type="submit"
+                    class="btn btn-md full-width bg-dark text-light fs-md ft-medium">{{__("Sign Up")}}</button>
             </div>
             <div>
-                <a href="{{ route('google.redirect') }}" class="btn btn-primary"> Register with Google </a>
-                <a href="{{ route('facebook.redirect') }}" class="btn btn-primary"> Register with Facebook </a>
+                <a href="{{ route('google.redirect') }}" class="btn btn-primary"> {{__("Login with Google")}} </a>
+                <a href="{{ route('facebook.redirect') }}" class="btn btn-primary"> {{__("Login with Facebook")}} </a>
             </div>
         </form>
         <div>
-            Already have an account? <a href="{{route('login')}}" class="text-dark">Login</a>
+            {{__("Already have an account?")}} <a href="{{route('login')}}" class="text-dark">{{__("Login")}}</a>
         </div>
     </div>
 </section>
