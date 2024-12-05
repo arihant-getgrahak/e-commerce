a4cd0d87765c395e0333c324e492a86911f6186b
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-12-05 16:50:25 +0530

    feat: add function to store language in db

--- app/Http/Controllers/LangController.php
+++ app/Http/Controllers/LangController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AddLanguageRequest;
+use App\Models\Language;
+use DB;
 use Illuminate\Http\Request;
 
 class LangController extends Controller
@@ -13,6 +16,29 @@ class LangController extends Controller
             session()->put('lang', $lang);
         } catch (\Exception $e) {
             dd($e);
+        }
+    }
+
+    public function languageStore(AddLanguageRequest $request)
+    {
+        try {
+            $data = [
+                'user_id' => auth()->user()->id,
+                'name' => $request->name,
+                'code' => $request->code,
+                'status' => $request->status,
+                'rtl' => $request->rtl,
+            ];
+
+            DB::beginTransaction();
+            Language::create($data);
+            DB::commit();
+
+            return redirect()->back()->with('success', 'Language added successfully');
+        } catch (\Exception $e) {
+            DB::rollBack();
+
+            return redirect()->back()->with('error', $e->getMessage());
         }
     }
 }
