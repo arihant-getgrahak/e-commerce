920ed8ae29220261d5437e21e938056a8c11ea40
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-12-06 20:57:26 +0530

    feat: optimize add translation func

--- resources/views/admin/setting/language-edit.blade.php
+++ resources/views/admin/setting/language-edit.blade.php
@@ -44,7 +44,8 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <form action="" method="POST">
+                <form action="{{route("admin.setting.language.add")}}" method="POST">
+                    @csrf
                     <input type="hidden" name="lang_code" value="{{$lang->code}}">
                     <div class="mb-3">
                         <label class="form-label required">Translation Key</label>
